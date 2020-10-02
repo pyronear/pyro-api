7e6509dfa511a2ac768df9129ef34fbef5d3ab2e
F-G Fernandez <fgfm03@hotmail.fr>
2020-10-02 12:48:09 +0200

    chore: Added user route

--- src/pyronear/urls.py
+++ src/pyronear/urls.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from pyronear.apps.user.resources import UserResource
+
+API_BASE = "pyronear/"
 
 urlpatterns = [
-    path('admin/', admin.site.urls)
+    path('admin/', admin.site.urls),
+    path(API_BASE, include(UserResource().urls))
 ]
