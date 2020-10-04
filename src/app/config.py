ac23c84132681037406e7879734e0f1c54df57d8
F-G Fernandez <fgfm03@hotmail.fr>
2020-10-04 13:49:58 +0200

    feat: Added server config

---
+++ src/app/config.py
@@ -0,0 +1,9 @@
+import os
+
+PROJECT_NAME: str = 'Pyronear API'
+PROJECT_DESCRIPTION: str = 'API for wildfire prevention, detection and monitoring'
+API_BASE: str = 'api/'
+VERSION: str = "0.1.0a0"
+DEBUG: bool = os.environ.get('DJANGO_DEBUG', '') != 'False'
+DATABASE_URL: str = os.getenv("DATABASE_URL")
+LOGO_URL: str = "https://github.com/pyronear/PyroNear/raw/master/docs/source/_static/img/pyronear-logo-dark.png"
