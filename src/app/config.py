faa64dafa2c44a693b4a1fc2afa4109b25695872
frgfm <fgfm03@hotmail.fr>
2020-10-10 12:13:01 +0200

    refactor: Renamed DEBUG config parameter

--- src/app/config.py
+++ src/app/config.py
@@ -4,6 +4,6 @@ PROJECT_NAME: str = 'Pyronear API'
 PROJECT_DESCRIPTION: str = 'API for wildfire prevention, detection and monitoring'
 API_BASE: str = 'api/'
 VERSION: str = "0.1.0a0"
-DEBUG: bool = os.environ.get('FASTAPI_DEBUG', '') != 'False'
+DEBUG: bool = os.environ.get('DEBUG', '') != 'False'
 DATABASE_URL: str = os.getenv("DATABASE_URL")
 LOGO_URL: str = "https://github.com/pyronear/PyroNear/raw/master/docs/source/_static/img/pyronear-logo-dark.png"
