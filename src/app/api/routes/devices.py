1e89474637ef6c42cc2d7337f0dbfdc766c5824a
florian <friche10@gmail.com>
2020-10-12 15:55:02 +0200

    feat: add heartbeat mecanism

--- src/app/api/routes/devices.py
+++ src/app/api/routes/devices.py
@@ -3,9 +3,12 @@ from fastapi import APIRouter, Path, Security
 
 from app.api import routing, crud
 from app.db import devices
+from app.api.schemas import Device, DeviceOut, DeviceIn, DeviceDBIn, HeartbeatOut, HeartbeatIn
+from app.security import get_password_hash
+from app.deps import get_current_active_device
+from datetime import datetime
 from app.api.schemas import DeviceOut, DeviceIn, UserInDb
 from app.api.deps import get_current_user
-
 
 router = APIRouter()
 
@@ -31,11 +34,11 @@ async def update_device(payload: DeviceIn, id: int = Path(..., gt=0)):
     return await routing.update_entry(devices, payload, id)
 
 
-@router.put("/{id}/heartbeat/")  # , response_model=HeartbeatOut)
-async def heartbeat(id: int = Path(..., gt=0), current_device: Device = Depends(get_current_active_device)):
-    # , payload=HeartbeatIn, id: int = Path(..., gt=0)): #payload: DeviceIn, id: int = Path(..., gt=0)):
-    return current_device  # await routing.update_entry(devices, payload, id)
-
+@router.put("/{id}/heartbeat/", response_model=HeartbeatOut)
+async def heartbeat(current_device: Device = Depends(get_current_active_device), id: int = Path(..., gt=0)):
+    current_device.last_ping = datetime.utcnow()
+    return await routing.update_entry(devices, Device(**current_device), id)
+    
 
 @router.delete("/{id}/", response_model=DeviceOut)
 async def delete_device(id: int = Path(..., gt=0), _=Security(get_current_user, scopes=["admin"])):
