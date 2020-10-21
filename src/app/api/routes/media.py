6b72524ae260ac97fbc7561e4eecf70aa6d731e6
florian <friche10@gmail.com>
2020-10-21 20:09:25 +0200

    feat: init image upload on S3

--- src/app/api/routes/media.py
+++ src/app/api/routes/media.py
@@ -1,17 +1,24 @@
 from typing import List
-from fastapi import APIRouter, Path
+from fastapi import APIRouter, Path, Security, File, UploadFile
 from app.api import routing
 from app.db import media
-from app.api.schemas import MediaOut, MediaIn
+from app.api.schemas import MediaOut, MediaIn, UserInDb
+from app.api.deps import get_current_user
+from app.services.bucketService import bucket_service
 
 
 router = APIRouter()
 
 
+@router.post("/upload_file", response_model=MediaOut, status_code=201)
+async def create_media(file: UploadFile = File(...)), device_user: UserInDb = Security(get_current_user, scopes=["device"]):
+    bucket_service.upload_file("mypyroneartest", file.filename, file.file)
+    MediaIn(device_id=device_user.id)
+    return await routing.create_entry(media, payload)
+
 @router.post("/", response_model=MediaOut, status_code=201)
 async def create_media(payload: MediaIn):
     return await routing.create_entry(media, payload)
-
 
 @router.get("/{id}/", response_model=MediaOut)
 async def get_media(id: int = Path(..., gt=0)):
