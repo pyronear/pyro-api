8287511a37105247f5746432b5cbfc74c17af09e
Jean <jpasquier@quantmetry.com>
2020-10-25 15:27:28 +0100

    test: conftest mocks 'connected_user' to handle user-auth. test_users & test_devices updated

--- src/tests/conftest.py
+++ src/tests/conftest.py
@@ -1,10 +1,23 @@
 import pytest
 from starlette.testclient import TestClient
+from datetime import datetime as dt
 
 from app.main import app
+from app.api.schemas import UserOut
+from app.api.deps import get_current_user
+
+
+async def mock_current_user():
+    return UserOut(id=99, username="connected_user", created_at=dt.now())
 
 
 @pytest.fixture(scope="module")
 def test_app():
+
+    app.dependency_overrides[get_current_user] = mock_current_user
+
     client = TestClient(app)
     yield client  # testing happens here
+
+    print("Tests done.")
+    app.dependency_overrides = {}
