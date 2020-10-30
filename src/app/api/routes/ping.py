edd7e2376c52a6c84aeda8e2b9b8b47e41e3a45c
F-G Fernandez <fgfm03@hotmail.fr>
2020-10-30 11:57:48 +0100

    docs: Removed useless docstring

--- src/app/api/routes/ping.py
+++ src/app/api/routes/ping.py
@@ -5,6 +5,4 @@ router = APIRouter()
 
 @router.get("/ping")
 async def pong():
-    # some async operation could happen here
-    # example: `notes = await get_all_notes()`
     return {"ping": "pong!"}
