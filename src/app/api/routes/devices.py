186175b8368aa79e340e365ef4d24fda9c1dee59
F-G Fernandez <fgfm03@hotmail.fr>
2020-11-11 23:36:05 +0100

    refactor: Reflected changes in schemas

--- src/app/api/routes/devices.py
+++ src/app/api/routes/devices.py
@@ -3,7 +3,7 @@ from fastapi import APIRouter, Path, Security
 
 from app.api import routing
 from app.db import devices
-from app.api.schemas import DeviceOut, DeviceAuth, DeviceIn, UserRead, HeartbeatOut, UpdatedLocation, Cred
+from app.api.schemas import DeviceOut, DeviceAuth, DeviceIn, UserRead, HeartbeatOut, DefaultPosition, Cred
 from app.api.deps import get_current_device, get_current_user
 
 router = APIRouter()
@@ -45,7 +45,7 @@ async def heartbeat(device: DeviceOut = Security(get_current_device, scopes=["de
 
 
 @router.post("/{device_id}/update-location", response_model=DeviceOut)
-async def update_location(payload: UpdatedLocation,
+async def update_location(payload: DefaultPosition,
                           user: UserRead = Security(get_current_user, scopes=["me"]),
                           device_id: int = Path(..., gt=0)):
     return await routing.update_location(devices, payload, device_id=id, user_id=user.id)
