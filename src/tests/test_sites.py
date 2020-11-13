fa526208be2a4d9a324c65f0a21704ea74e34d0c
F-G Fernandez <fgfm03@hotmail.fr>
2020-11-13 01:07:35 +0100

    test: Updated site unittests

--- src/tests/test_sites.py
+++ src/tests/test_sites.py
@@ -11,84 +11,104 @@ def test_create_site(test_app, monkeypatch):
     test_request_payload = FULL_PAYLOAD
     test_response_payload = {"id": 1, **FULL_PAYLOAD}
 
+    # Sterilize DB interactions
     async def mock_post(payload, table):
         return 1
 
     monkeypatch.setattr(crud, "post", mock_post)
 
-    response = test_app.post("/sites/", data=json.dumps(test_request_payload),)
+    response = test_app.post("/sites/", data=json.dumps(test_request_payload))
 
     assert response.status_code == 201
     assert {k: v for k, v in response.json().items() if k != 'created_at'} == test_response_payload
 
 
-def test_create_site_invalid_json(test_app):
-    response = test_app.post("/sites/", data=json.dumps({"names": "my_site", "lat": 0., "lon": 0., "type": "tower"}))
-    assert response.status_code == 422
-
-    response = test_app.post("/sites/", data=json.dumps({"name": "my_site", "lat": 0.}))
-    assert response.status_code == 422
+@pytest.mark.parametrize(
+    "payload, status_code",
+    [
+        [{"names": "my_site", "lat": 0., "lon": 0., "type": "tower"}, 422],
+        [{"name": "my_site", "lat": 0.}, 422],
+    ],
+)
+def test_create_site_invalid_json(test_app, payload, status_code):
+    response = test_app.post("/sites/", data=json.dumps(payload))
+    assert response.status_code == status_code
 
 
 def test_get_site(test_app, monkeypatch):
-    test_data = {"id": 1, **FULL_PAYLOAD}
+    test_table = [{"id": 1, **FULL_PAYLOAD}]
 
     async def mock_get(entry_id, table):
-        return test_data
+        for entry in test_table:
+            if entry['id'] == entry_id:
+                return entry
+        return None
 
     monkeypatch.setattr(crud, "get", mock_get)
 
     response = test_app.get("/sites/1")
     assert response.status_code == 200
-    assert {k: v for k, v in response.json().items() if k != 'created_at'} == test_data
+    assert {k: v for k, v in response.json().items() if k != 'created_at'} == test_table[0]
 
 
-def test_get_site_incorrect_id(test_app, monkeypatch):
+@pytest.mark.parametrize(
+    "site_id, status_code, status_details",
+    [
+        [999, 404, "Entry not found"],
+        [0, 422, None],
+    ],
+)
+def test_get_site_incorrect_id(test_app, monkeypatch, site_id, status_code, status_details):
+    test_table = [{"id": 1, **FULL_PAYLOAD}]
     async def mock_get(entry_id, table):
+        for entry in test_table:
+            if entry['id'] == entry_id:
+                return entry
         return None
 
     monkeypatch.setattr(crud, "get", mock_get)
 
-    response = test_app.get("/sites/999")
-    assert response.status_code == 404
-    assert response.json()["detail"] == "Entry not found"
-
-    response = test_app.get("/sites/0")
-    assert response.status_code == 422
+    response = test_app.get(f"/sites/{site_id}")
+    assert response.status_code == status_code, site_id
+    if isinstance(status_details, str):
+        assert response.json()["detail"] == status_details
 
 
 def test_fetch_sites(test_app, monkeypatch):
-    test_data = [
+    test_table = [
         {"id": 1, **FULL_PAYLOAD},
         {"id": 2, **FULL_PAYLOAD},
     ]
 
-    async def mock_get_all(table, query_filter=None):
-        return test_data
+    async def mock_fetch_all(table, query_filter=None):
+        return test_table
 
-    monkeypatch.setattr(crud, "fetch_all", mock_get_all)
+    monkeypatch.setattr(crud, "fetch_all", mock_fetch_all)
 
     response = test_app.get("/sites/")
     assert response.status_code == 200
-    assert [{k: v for k, v in r.items() if k != 'created_at'} for r in response.json()] == test_data
+    assert [{k: v for k, v in r.items() if k != 'created_at'} for r in response.json()] == test_table
 
 
 def test_update_site(test_app, monkeypatch):
-    test_update_data = {"id": 1, **FULL_PAYLOAD}
+    test_table = [{"id": 1, **FULL_PAYLOAD}]
 
     async def mock_get(entry_id, table):
-        return True
+        for entry in test_table:
+            if entry['id'] == entry_id:
+                return entry
+        return None
 
     monkeypatch.setattr(crud, "get", mock_get)
 
     async def mock_put(entry_id, payload, table):
-        return 1
+        return entry_id
 
     monkeypatch.setattr(crud, "put", mock_put)
 
-    response = test_app.put("/sites/1/", data=json.dumps(test_update_data))
+    response = test_app.put("/sites/1/", data=json.dumps(test_table[0]))
     assert response.status_code == 200
-    assert {k: v for k, v in response.json().items() if k != 'created_at'} == test_update_data
+    assert {k: v for k, v in response.json().items() if k != 'created_at'} == test_table[0]
 
 
 @pytest.mark.parametrize(
@@ -102,12 +122,21 @@ def test_update_site(test_app, monkeypatch):
     ],
 )
 def test_update_site_invalid(test_app, monkeypatch, site_id, payload, status_code):
+    test_table = [{"id": 1, **FULL_PAYLOAD}]
     async def mock_get(entry_id, table):
+        for entry in test_table:
+            if entry['id'] == entry_id:
+                return entry
         return None
 
     monkeypatch.setattr(crud, "get", mock_get)
 
-    response = test_app.put(f"/sites/{site_id}/", data=json.dumps(payload),)
+    async def mock_put(entry_id, payload, table):
+        return entry_id
+
+    monkeypatch.setattr(crud, "put", mock_put)
+
+    response = test_app.put(f"/sites/{site_id}/", data=json.dumps(payload))
     assert response.status_code == status_code, print(payload)
 
 
@@ -129,15 +158,20 @@ def test_remove_site(test_app, monkeypatch):
     assert {k: v for k, v in response.json().items() if k != 'created_at'} == test_data
 
 
-def test_remove_site_incorrect_id(test_app, monkeypatch):
+@pytest.mark.parametrize(
+    "site_id, status_code, status_details",
+    [
+        [999, 404, "Entry not found"],
+        [0, 422, None],
+    ],
+)
+def test_remove_site_incorrect_id(test_app, monkeypatch, site_id, status_code, status_details):
     async def mock_get(entry_id, table):
         return None
 
     monkeypatch.setattr(crud, "get", mock_get)
 
-    response = test_app.delete("/sites/999/")
-    assert response.status_code == 404
-    assert response.json()["detail"] == "Entry not found"
-
-    response = test_app.delete("/sites/0/")
-    assert response.status_code == 422
+    response = test_app.delete(f"/sites/{site_id}/")
+    assert response.status_code == status_code
+    if isinstance(status_details, str):
+        assert response.json()["detail"] == status_details
