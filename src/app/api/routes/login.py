9b27c1ed3b4c1f9fc0911edf0998a3436bb04e8b
F-G Fernandez <fgfm03@hotmail.fr>
2020-11-13 15:54:07 +0100

    test: Refactored login route unittests

--- src/app/api/routes/login.py
+++ src/app/api/routes/login.py
@@ -11,14 +11,14 @@ router = APIRouter()
 
 
 @router.post("/access-token", response_model=Token)
-async def login_access_token(form_data: OAuth2PasswordRequestForm = Depends()):
+async def create_access_token(form_data: OAuth2PasswordRequestForm = Depends()):
+    # Verify credentials
     entry = await routing.fetch_entry(accesses, [('login', form_data.username)])
     if entry is None or not await security.verify_password(form_data.password, entry['hashed_password']):
-        raise HTTPException(status_code=400, detail="Incorrect login or password")
+        raise HTTPException(status_code=400, detail="Invalid credentials")
     # create access token using user user_id/user_scopes
     token_data = {"sub": str(entry['id']), "scopes": entry['scopes'].split()}
-    is_device = entry["scopes"] == "device"
-    if is_device:
+    if entry["scopes"] == "device":
         token = await security.create_unlimited_access_token(token_data)
     else:
         token = await security.create_access_token(token_data,
--- src/tests/test_login.py
+++ src/tests/test_login.py
@@ -3,40 +3,42 @@ import pytest
 from app.api import crud, security
 
 
+MOCK_TABLE = [
+    {"id": 1, "login": "first_login", "hashed_password": "first_pwd_hashed", "scopes": "me"},
+    {"id": 2, "login": "second_login", "hashed_password": "second_pwd_hashed", "scopes": "me"},
+]
+
+
 @pytest.mark.parametrize(
-    "payload, status_code",
+    "payload, status_code, status_detail",
     [
-        [{"username": "foo"}, 422],
-        [{"password": "foo"}, 422],
-        [{"username": "unknown", "password": "foo"}, 400],  # unknown username
-        [{"username": "first", "password": "first"}, 200],  # valid
-        [{"username": "first", "password": "second"}, 400],  # wrong pwd
+        [{"username": "foo"}, 422, None],
+        [{"password": "foo"}, 422, None],
+        [{"username": "unknown", "password": "foo"}, 400, "Invalid credentials"],  # unknown username
+        [{"username": "first", "password": "second"}, 400, "Invalid credentials"],  # wrong pwd
+        [{"username": "first_login", "password": "first_pwd"}, 200, None],  # valid
     ],
 )
-def test_access_token(test_app, monkeypatch, payload, status_code):
+def test_create_access_token(test_app, monkeypatch, payload, status_code, status_detail):
 
-    test_data = [
-        {"login": "first", "hashed_password": "first_hashed", "scopes": "me", "id": 1},
-        {"login": "second", "hashed_password": "second_hashed", "scopes": "me", "id": 2},
-        {"login": "third", "hashed_password": "third_hashed", "scopes": "me admin", "id": 3},
-        {"login": "fourth", "hashed_password": "fourth_hashed", "scopes": "device", "id": 4},
-    ]
-
+    # Sterilize DB interactions
+    local_db = MOCK_TABLE.copy()
     async def mock_fetch_one(table, query_filters):
-        for entry in test_data:
-            for query_filter in query_filters:
-                valid_entry = True
-                if entry[query_filter[0]] != query_filter[1]:
-                    valid_entry = False
-            if valid_entry:
+        for entry in local_db:
+            if all(entry[k] == v for k, v in query_filters):
                 return entry
+        return None
 
     monkeypatch.setattr(crud, "fetch_one", mock_fetch_one)
 
+    # Override cred verification
     async def verify_password(plain_password, hashed_password):
         return hashed_password == f"{plain_password}_hashed"
 
     monkeypatch.setattr(security, "verify_password", verify_password)
 
     response = test_app.post("/login/access-token", data=payload)
+
     assert response.status_code == status_code, print(payload)
+    if isinstance(status_detail, str):
+        assert response.json()['detail'] == status_detail
