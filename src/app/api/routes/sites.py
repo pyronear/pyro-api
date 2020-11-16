3e83c699db10c5145f83be7da45946e773003dfe
fe <veith.felix@gmail.com>
2020-11-16 21:34:08 +0100

    docs: rewording

--- src/app/api/routes/sites.py
+++ src/app/api/routes/sites.py
@@ -11,12 +11,12 @@ router = APIRouter()
 @router.post("/", response_model=SiteOut, status_code=201, summary="Create a new site")
 async def create_site(payload: SiteIn):
     """
-    Create a new site based on the given information:
+    Creates a new site based on the given information:
 
-    - **lon**: longitude of the site
-    - **lat**: latitude of the site
-    - **name**: name
-    - **type**: type of the site chosen in {"tower", "station"}
+    - **lon**: Longitude of the site
+    - **lat**: Latitude of the site
+    - **name**: Site name
+    - **type**: Type of the site chosen in the following set {"tower", "station"}
 
     Below, click on "Schema" for more detailed information about fields
     or "Example Value" to get a concrete idea of inputs fields
@@ -24,10 +24,10 @@ async def create_site(payload: SiteIn):
     return await routing.create_entry(sites, payload)
 
 
-@router.get("/{site_id}/", response_model=SiteOut, summary="Get information of a given site")
+@router.get("/{site_id}/", response_model=SiteOut, summary="Get information about a specific site")
 async def get_site(site_id: int = Path(..., gt=0)):
     """
-    Based on a site_id, get information about the given site
+    Based on a site_id, retrieves information about the given site
     """
     return await routing.get_entry(sites, site_id)
 
@@ -35,22 +35,22 @@ async def get_site(site_id: int = Path(..., gt=0)):
 @router.get("/", response_model=List[SiteOut], summary="Get the list of all sites")
 async def fetch_sites():
     """
-    Get the list of all sites with each related information
+    Retrieves the list of all sites with each related information
     """
     return await routing.fetch_entries(sites)
 
 
-@router.put("/{site_id}/", response_model=SiteOut, summary="Update information of a given site")
+@router.put("/{site_id}/", response_model=SiteOut, summary="Update information about a specific site")
 async def update_site(payload: SiteIn, site_id: int = Path(..., gt=0)):
     """
-    Based on a site_id, update information about the given site
+    Based on a site_id, updates information about the given site
     """
     return await routing.update_entry(sites, payload, site_id)
 
 
-@router.delete("/{site_id}/", response_model=SiteOut, summary="Delete a given site")
+@router.delete("/{site_id}/", response_model=SiteOut, summary="Delete a specific site")
 async def delete_site(site_id: int = Path(..., gt=0)):
     """
-    Based on a site_id, delete the given site
+    Based on a site_id, deletes the given site
     """
     return await routing.delete_entry(sites, site_id)
