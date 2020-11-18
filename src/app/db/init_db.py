39b4a10518eff4a76373dd70f01f40b5ecbd36e1
fe <veith.felix@gmail.com>
2020-11-18 18:53:18 +0100

    fix: remove and replace some missing cfg callsl

--- src/app/db/init_db.py
+++ src/app/db/init_db.py
@@ -1,4 +1,5 @@
 import os
+
 from app.api import crud
 from app.db import accesses, users
 from app.api.schemas import AccessCreation, UserCreation
@@ -7,13 +8,17 @@ from app.api.security import hash_password
 
 async def init_db():
 
+    login = os.getenv("SUPERUSER_LOGIN")
+
     # check if access login does not already exist
-    entry = await crud.fetch_one(accesses, [("login", cfg.SUPERUSER_LOGIN)])
+    entry = await crud.fetch_one(accesses, [("login", login)])
     if entry is None:
 
         hashed_password = await hash_password(os.getenv("SUPERUSER_PWD"))
 
-        access = AccessCreation(login=os.getenv("SUPERUSER_LOGIN"), hashed_password=hashed_password, scopes="admin")
+        access = AccessCreation(login=login, hashed_password=hashed_password, scopes="admin")
         access_entry = await crud.create_entry(accesses, access)
 
-    return await crud.create_entry(users, UserCreation(login=cfg.SUPERUSER_LOGIN, access_id=access_entry["id"]))
+        await crud.create_entry(users, UserCreation(login=login, access_id=access_entry["id"]))
+
+    return None
