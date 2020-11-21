28c64e081c358d8ee455c4069adbf64953972f6f
Unknown <friche10@gmail.com>
2020-11-21 20:42:11 +0100

    move assert check to unit test

--- src/app/services/services.py
+++ src/app/services/services.py
@@ -2,6 +2,5 @@ from app.services.bucket.s3 import S3Service
 from app.services.bucket.baseBucketService import BaseBucketService
 
 bucket_service = S3Service()
-assert isinstance(bucket_service, BaseBucketService), "selected service is not an instance of BaseBucketService"
 
 __all__ = ['bucket_service']
