56bb443b82ccfe623fbc535cad8879a2d602d452
Jean <jpasquier@quantmetry.com>
2020-11-22 23:51:03 +0100

    fix: crud db fetch return abstract Mapping, get entries return Dict

--- src/app/api/crud.py
+++ src/app/api/crud.py
@@ -1,4 +1,4 @@
-from typing import Optional, Any, List, Dict
+from typing import Optional, Any, List, Dict, Mapping
 from app.db import database
 from sqlalchemy import Table
 from pydantic import BaseModel
@@ -10,12 +10,12 @@ async def post(payload: BaseModel, table: Table) -> int:
     return await database.execute(query=query)
 
 
-async def get(entry_id: int, table: Table) -> Dict[str, Any]:
+async def get(entry_id: int, table: Table) -> Mapping[str, Any]:
     query = table.select().where(entry_id == table.c.id)
     return await database.fetch_one(query=query)
 
 
-async def fetch_all(table: Table, query_filters: Optional[Dict[str, Any]] = None) -> List[Dict[str, Any]]:
+async def fetch_all(table: Table, query_filters: Optional[Dict[str, Any]] = None) -> List[Mapping[str, Any]]:
     query = table.select()
     if isinstance(query_filters, dict):
         for query_filter_key, query_filter_value in query_filters.items():
@@ -23,7 +23,7 @@ async def fetch_all(table: Table, query_filters: Optional[Dict[str, Any]] = None
     return await database.fetch_all(query=query)
 
 
-async def fetch_one(table: Table, query_filters: Dict[str, Any]) -> Dict[str, Any]:
+async def fetch_one(table: Table, query_filters: Dict[str, Any]) -> Mapping[str, Any]:
     query = table.select()
     for query_filter_key, query_filter_value in query_filters.items():
         query = query.where(getattr(table.c, query_filter_key) == query_filter_value)
@@ -55,7 +55,8 @@ async def get_entry(table: Table, entry_id: int = Path(..., gt=0)) -> Dict[str, 
     entry = await get(entry_id, table)
     if entry is None:
         raise HTTPException(status_code=404, detail="Entry not found")
-    return entry
+
+    return dict(**entry)
 
 
 async def update_entry(table: Table, payload: BaseModel, entry_id: int = Path(..., gt=0)) -> Dict[str, Any]:
