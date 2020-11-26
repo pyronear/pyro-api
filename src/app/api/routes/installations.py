1b5b88cbbab57be3b0b48f8e21977f2c5473dcc0
martin1tab <61795801+martin1tab@users.noreply.github.com>
2020-11-26 13:48:28 +0100

    feat: Adding a route to list of devices on a site at a given time  (#56)
    * add route list_devices
    
    * add list_devices tests
    
    * fix some issues
    
    * fix issues
    
    * fix issues2
    
    * fix issues
    
    * Delete .Rhistory

--- src/app/api/routes/installations.py
+++ src/app/api/routes/installations.py
@@ -1,8 +1,9 @@
 from typing import List
 from fastapi import APIRouter, Path
+from sqlalchemy import and_
 from app.api import crud
-from app.db import installations
-from app.api.schemas import InstallationOut, InstallationIn
+from app.db import installations, database
+from app.api.schemas import InstallationOut, InstallationIn, Timestamp
 
 
 router = APIRouter()
@@ -31,3 +32,12 @@ async def update_installation(payload: InstallationIn, installation_id: int = Pa
 @router.delete("/{installation_id}/", response_model=InstallationOut)
 async def delete_installation(installation_id: int = Path(..., gt=0)):
     return await crud.delete_entry(installations, installation_id)
+
+
+@router.post("/site-devices/{site_id}", response_model=List[int])
+async def get_all_at_given_ts_and_site(payload: Timestamp, site_id: int = Path(..., gt=0)):
+
+    query = installations.select(installations.c.device_id).where(and_(installations.c.site_id == site_id,
+                                                                       installations.c.start_ts <= timestamp,
+                                                                       installations.c.end_ts >= timestamp))
+    return await database.fetch_all(query=query)
