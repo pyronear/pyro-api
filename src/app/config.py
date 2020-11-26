3ab3ddbe576ae7236a71d3886d20a0f69e477a07
F-G Fernandez <fgfm03@hotmail.fr>
2020-11-26 18:40:34 +0100

    chore: Updated API & client versions

--- src/app/config.py
+++ src/app/config.py
@@ -5,7 +5,7 @@ import secrets
 PROJECT_NAME: str = 'Pyronear API'
 PROJECT_DESCRIPTION: str = 'API for wildfire prevention, detection and monitoring'
 API_BASE: str = 'api/'
-VERSION: str = "0.1.0a0"
+VERSION: str = "0.1.1a0"
 DEBUG: bool = os.environ.get('DEBUG', '') != 'False'
 DATABASE_URL: str = os.getenv("DATABASE_URL")
 LOGO_URL: str = "https://github.com/pyronear/PyroNear/raw/master/docs/source/_static/img/pyronear-logo-dark.png"
