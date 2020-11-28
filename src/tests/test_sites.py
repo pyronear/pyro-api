e636a1c11ee7cdd99780609cf452a9f33ccd8e00
Unknown <friche10@gmail.com>
2020-11-28 20:27:30 +0100

    refactor site tests

--- src/tests/test_sites.py
+++ src/tests/test_sites.py
@@ -3,9 +3,10 @@ import pytest
 from copy import deepcopy
 from datetime import datetime
 
+from app import db
 from app.api import crud
 from app.api.routes import sites
-
+from tests.conf_test_db import get_entry_in_db, populate_db
 
 SITE_TABLE = [
     {"id": 1, "name": "my_first_tower", "lat": 44.1, "lon": -0.7, "type": "tower", "country": "FR", "geocode": "40",
@@ -14,27 +15,42 @@ SITE_TABLE = [
      "created_at": "2020-09-13T08:18:45.447773"},
 ]
 
-
-def _patch_session(monkeypatch, mock_table):
-    # DB patching
-    monkeypatch.setattr(sites, "sites", mock_table)
-    # Sterilize all DB interactions through CRUD override
-    monkeypatch.setattr(crud, "get", pytest.mock_get)
-    monkeypatch.setattr(crud, "fetch_all", pytest.mock_fetch_all)
-    monkeypatch.setattr(crud, "post", pytest.mock_post)
-    monkeypatch.setattr(crud, "put", pytest.mock_put)
-    monkeypatch.setattr(crud, "delete", pytest.mock_delete)
+DATETIME_FORMAT = "%Y-%m-%dT%H:%M:%S.%f"
 
 
-def test_get_site(test_app, monkeypatch):
+def compare_entries(ref, test):
+    for k, v in ref.items():
+        if isinstance(v, float):
+            # For float issues
+            assert abs(v - test[k]) < 1E-5
+        else:
+            assert v == test[k]
+
+def update_only_datetime(entity_as_dict):
+    to_return = entity_as_dict.copy()
+    if "created_at" in to_return:
+        to_return["created_at"] = datetime.strptime(to_return["created_at"], DATETIME_FORMAT)
+    return to_return
+
+
+SITE_TABLE_FOR_DB = list(map(update_only_datetime, SITE_TABLE))
+
+
+async def init_test_db(monkeypatch, test_db):
+    monkeypatch.setattr(crud, "database", test_db)
+    await populate_db(test_db, db.sites, SITE_TABLE_FOR_DB)
+
+
+@pytest.mark.asyncio
+async def test_get_site(test_app_asyncio, test_db, monkeypatch):
 
     # Sterilize DB interactions
-    mock_site_table = deepcopy(SITE_TABLE)
-    _patch_session(monkeypatch, mock_site_table)
+    await init_test_db(monkeypatch, test_db)
 
-    response = test_app.get("/sites/1")
+    response = await test_app_asyncio.get("/sites/1")
+    response_json = response.json()
     assert response.status_code == 200
-    assert response.json() == mock_site_table[0]
+    compare_entries(response_json, SITE_TABLE[0])
 
 
 @pytest.mark.parametrize(
@@ -44,43 +60,47 @@ def test_get_site(test_app, monkeypatch):
         [0, 422, None],
     ],
 )
-def test_get_site_invalid(test_app, monkeypatch, site_id, status_code, status_details):
+@pytest.mark.asyncio
+async def test_get_site_invalid(test_app_asyncio, test_db, monkeypatch, site_id, status_code, status_details):
     # Sterilize DB interactions
-    mock_site_table = deepcopy(SITE_TABLE)
-    _patch_session(monkeypatch, mock_site_table)
+    await init_test_db(monkeypatch, test_db)
 
-    response = test_app.get(f"/sites/{site_id}")
+    response = await test_app_asyncio.get(f"/sites/{site_id}")
     assert response.status_code == status_code, site_id
     if isinstance(status_details, str):
         assert response.json()["detail"] == status_details
 
 
-def test_fetch_sites(test_app, monkeypatch):
+@pytest.mark.asyncio
+async def test_fetch_sites(test_app_asyncio, test_db, monkeypatch):
     # Sterilize DB interactions
-    mock_site_table = deepcopy(SITE_TABLE)
-    _patch_session(monkeypatch, mock_site_table)
+    await init_test_db(monkeypatch, test_db)
 
-    response = test_app.get("/sites/")
+    response = await test_app_asyncio.get("/sites/")
     assert response.status_code == 200
-    assert response.json() == mock_site_table
+    response_json = response.json()
+    for (i, entry) in enumerate(response_json):
+        compare_entries(entry, SITE_TABLE[i])
 
 
-def test_create_site(test_app, monkeypatch):
+@pytest.mark.asyncio
+async def test_create_site(test_app_asyncio, test_db, monkeypatch):
 
     # Sterilize DB interactions
-    mock_site_table = deepcopy(SITE_TABLE)
-    _patch_session(monkeypatch, mock_site_table)
+    await init_test_db(monkeypatch, test_db)
 
     test_payload = {"name": "my_site", "lat": 0., "lon": 0., "type": "tower", "country": "FR", "geocode": "01"}
-    test_response = {"id": len(mock_site_table) + 1, **test_payload}
+    test_response = {"id": len(SITE_TABLE) + 1, **test_payload}
 
     utc_dt = datetime.utcnow()
-    response = test_app.post("/sites/", data=json.dumps(test_payload))
+    response = await test_app_asyncio.post("/sites/", data=json.dumps(test_payload))
 
     assert response.status_code == 201
     json_response = response.json()
     assert {k: v for k, v in json_response.items() if k != 'created_at'} == test_response
-    assert mock_site_table[-1]['created_at'] > utc_dt and mock_site_table[-1]['created_at'] < datetime.utcnow()
+    new_site_in_db = await get_entry_in_db(test_db, db.sites, json_response["id"])
+    new_site_in_db = dict(**new_site_in_db)
+    assert new_site_in_db['created_at'] > utc_dt and new_site_in_db['created_at'] < datetime.utcnow()
 
 
 @pytest.mark.parametrize(
@@ -90,21 +110,24 @@ def test_create_site(test_app, monkeypatch):
         [{"name": "my_site", "lat": 0., "country": "FR", "geocode": "01"}, 422],
     ],
 )
-def test_create_site_invalid(test_app, payload, status_code):
-    response = test_app.post("/sites/", data=json.dumps(payload))
+@pytest.mark.asyncio
+async def test_create_site_invalid(test_app_asyncio, test_db, payload, status_code):
+    response = await test_app_asyncio.post("/sites/", data=json.dumps(payload))
     assert response.status_code == status_code
 
 
-def test_update_site(test_app, monkeypatch):
+@pytest.mark.asyncio
+async def test_update_site(test_app_asyncio, test_db, monkeypatch):
     # Sterilize DB interactions
-    mock_site_table = deepcopy(SITE_TABLE)
-    _patch_session(monkeypatch, mock_site_table)
+    await init_test_db(monkeypatch, test_db)
 
     test_payload = {"name": "renamed_site", "lat": 0., "lon": 0., "type": "tower", "country": "FR", "geocode": "01"}
-    response = test_app.put("/sites/1/", data=json.dumps(test_payload))
+    response = await test_app_asyncio.put("/sites/1/", data=json.dumps(test_payload))
     assert response.status_code == 200
-    for k, v in mock_site_table[0].items():
-        assert v == test_payload.get(k, SITE_TABLE[0][k])
+    updated_site_in_db = await get_entry_in_db(test_db, db.sites, 1)
+    updated_site_in_db = dict(**updated_site_in_db)
+    for k, v in updated_site_in_db.items():
+        assert v == test_payload.get(k, SITE_TABLE_FOR_DB[0][k])
 
 
 @pytest.mark.parametrize(
@@ -117,24 +140,26 @@ def test_update_site(test_app, monkeypatch):
         [0, {"name": "foo", "lat": 0., "lon": 0., "type": "tower", "country": "FR", "geocode": "01"}, 422],
     ],
 )
-def test_update_site_invalid(test_app, monkeypatch, site_id, payload, status_code):
+@pytest.mark.asyncio
+async def test_update_site_invalid(test_app_asyncio, test_db, monkeypatch, site_id, payload, status_code):
     # Sterilize DB interactions
-    mock_site_table = deepcopy(SITE_TABLE)
-    _patch_session(monkeypatch, mock_site_table)
+    await init_test_db(monkeypatch, test_db)
 
-    response = test_app.put(f"/sites/{site_id}/", data=json.dumps(payload))
+    response = await test_app_asyncio.put(f"/sites/{site_id}/", data=json.dumps(payload))
     assert response.status_code == status_code, print(payload)
 
 
-def test_delete_site(test_app, monkeypatch):
+@pytest.mark.asyncio
+async def test_delete_site(test_app_asyncio, test_db, monkeypatch):
     # Sterilize DB interactions
-    mock_site_table = deepcopy(SITE_TABLE)
-    _patch_session(monkeypatch, mock_site_table)
+    await init_test_db(monkeypatch, test_db)
 
-    response = test_app.delete("/sites/1/")
+    response = await test_app_asyncio.delete("/sites/1/")
     assert response.status_code == 200
-    assert response.json() == SITE_TABLE[0]
-    for entry in mock_site_table:
+    compare_entries(response.json(), SITE_TABLE[0])
+    
+    remaining_sites = await test_app_asyncio.get("/sites/")
+    for entry in remaining_sites.json():
         assert entry['id'] != 1
 
 
@@ -145,12 +170,12 @@ def test_delete_site(test_app, monkeypatch):
         [0, 422, None],
     ],
 )
-def test_delete_site_invalid(test_app, monkeypatch, site_id, status_code, status_details):
+@pytest.mark.asyncio
+async def test_delete_site_invalid(test_app_asyncio, test_db, monkeypatch, site_id, status_code, status_details):
     # Sterilize DB interactions
-    mock_site_table = deepcopy(SITE_TABLE)
-    _patch_session(monkeypatch, mock_site_table)
+    await init_test_db(monkeypatch, test_db)
 
-    response = test_app.delete(f"/sites/{site_id}/")
+    response = await test_app_asyncio.delete(f"/sites/{site_id}/")
     assert response.status_code == status_code, print(site_id)
     if isinstance(status_details, str):
         assert response.json()["detail"] == status_details, print(site_id)
