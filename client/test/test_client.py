8b04b113744a3899983ba55f05bd1c5030a9d81a
F-G Fernandez <fgfm03@hotmail.fr>
2020-11-29 17:37:20 +0100

    test: Added basic unittests for client

--- client/test/test_client.py
+++ client/test/test_client.py
@@ -1,13 +1,26 @@
 import unittest
 
 from pyroclient import client
+from pyroclient.exceptions import HTTPRequestException
 
 
 class ClientTester(unittest.TestCase):
 
     def test_client(self):
 
-        self.assertRaises(Exception, client.Client, "http://localhost:8002", "invalid_login", "invalid_pwd")
+        # Wrong credentials
+        self.assertRaises(HTTPRequestException, client.Client, "http://localhost:8002", "invalid_login", "invalid_pwd")
+
+        # Correct creds
+        api_client = client.Client("http://localhost:8002", "superuser", "superuser")
+
+        # Read routes
+        response = api_client.get_sites()
+        self.assertEqual(response.status_code, 200)
+        self.assertIsInstance(response.json(), list)
+        response = api_client.get_all_alerts()
+        self.assertEqual(response.status_code, 200)
+        self.assertIsInstance(response.json(), list)
 
 
 if __name__ == '__main__':
