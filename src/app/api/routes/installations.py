129ff9da2783c58b55faad042be5a5f1cf65efea
fe51 <55736935+fe51@users.noreply.github.com>
2020-12-02 00:21:01 +0100

    [docs] Add route description and summary to remaining routes (#78)
    * docs: add summary and desc to installations routes
    
    * docs: correct typo
    
    * docs: add summary and desc to devices routes
    
    * docs: add summary and desc to users routes
    
    * docs: add summary and desc to alerts routes
    
    * docs: add summary and des to accesses routes
    
    * correct typo
    
    * fix conflicts
    
    Co-authored-by: F-G Fernandez <fgfm03@hotmail.fr>

--- src/app/api/routes/installations.py
+++ src/app/api/routes/installations.py
@@ -9,34 +9,56 @@ from app.api.schemas import InstallationOut, InstallationIn, Timestamp
 router = APIRouter()
 
 
-@router.post("/", response_model=InstallationOut, status_code=201)
+@router.post("/", response_model=InstallationOut, status_code=201, summary="Create a new installation")
 async def create_installation(payload: InstallationIn):
+    """Creates a new installation based on the given information
+
+    Below, click on "Schema" for more detailed information about arguments
+    or "Example Value" to get a concrete idea of arguments
+    """
     return await crud.create_entry(installations, payload)
 
 
-@router.get("/{installation_id}/", response_model=InstallationOut)
+@router.get("/{installation_id}/", response_model=InstallationOut,
+            summary="Get information about a specific installation")
 async def get_installation(installation_id: int = Path(..., gt=0)):
+    """
+    Based on a installation_id, retrieves information about the specified installation
+    """
     return await crud.get_entry(installations, installation_id)
 
 
-@router.get("/", response_model=List[InstallationOut])
+@router.get("/", response_model=List[InstallationOut], summary="Get the list of all installations")
 async def fetch_installations():
+    """
+    Retrieves the list of all installations and their information
+    """
     return await crud.fetch_all(installations)
 
 
-@router.put("/{installation_id}/", response_model=InstallationOut)
+@router.put("/{installation_id}/", response_model=InstallationOut,
+            summary="Update information about a specific installation")
 async def update_installation(payload: InstallationIn, installation_id: int = Path(..., gt=0)):
+    """
+    Based on a installation_id, updates information about the specified installation
+    """
     return await crud.update_entry(installations, payload, installation_id)
 
 
-@router.delete("/{installation_id}/", response_model=InstallationOut)
+@router.delete("/{installation_id}/", response_model=InstallationOut, summary="Delete a specific installation")
 async def delete_installation(installation_id: int = Path(..., gt=0)):
+    """
+    Based on a installation_id, deletes the specified installation
+    """
     return await crud.delete_entry(installations, installation_id)
 
 
-@router.get("/site-devices/{site_id}", response_model=List[int])
+@router.post("/site-devices/{site_id}", response_model=List[int],
+             summary="Get all devices related to a specific site")
 async def get_all_at_given_ts_and_site(payload: Timestamp, site_id: int = Path(..., gt=0)):
-
+    """
+    Based on a site_id, retrieves the list of all the related devices and their information
+    """
     query = installations.select(installations.c.device_id).where(and_(installations.c.site_id == site_id,
                                                                        installations.c.start_ts <= payload.timestamp,
                                                                        installations.c.end_ts >= payload.timestamp))
