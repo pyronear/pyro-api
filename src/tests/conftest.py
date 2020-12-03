d6e6a79846b82ef4500d1470014d3d4ebd12ce28
Florian Riche <friche10@gmail.com>
2020-12-03 15:48:20 +0100

    BUGFIX: Events route accesses as device (#85)
    * fix: replace get_current_user by get_current_access when expected scopes are both devices and Admin
    
    * fix: add missing unit tests mockup

--- src/tests/conftest.py
+++ src/tests/conftest.py
@@ -3,8 +3,8 @@ from starlette.testclient import TestClient
 from datetime import datetime
 
 from app.main import app
-from app.api.schemas import UserRead, DeviceOut
-from app.api.deps import get_current_user, get_current_device
+from app.api.schemas import UserRead, DeviceOut, AccessRead
+from app.api.deps import get_current_user, get_current_device, get_current_access
 from tests.conf_test_db import database as test_database
 from tests.conf_test_db import reset_test_db
 from httpx import AsyncClient
@@ -12,6 +12,10 @@ from httpx import AsyncClient
 
 async def mock_current_user():
     return UserRead(id=2, login="connected_user", created_at=datetime.now())
+
+
+async def mock_current_access():
+    return AccessRead(id=2, login="connected_user", scopes="device", created_at=datetime.now())
 
 
 async def mock_current_device():
@@ -38,6 +42,7 @@ def __app():
     # Access-related patching
     app.dependency_overrides[get_current_user] = mock_current_user
     app.dependency_overrides[get_current_device] = mock_current_device
+    app.dependency_overrides[get_current_access] = mock_current_access
 
     yield app  # testing happens here
 
