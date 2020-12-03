593e1d799db633a32549ee064dbdcd86f69daa3b
martin1tab <martin.tabikh@gmail.com>
2020-12-03 20:52:29 +0100

    Add Qarnot storage

--- src/app/services/bucket/qarnotBucketService.py
+++ src/app/services/bucket/qarnotBucketService.py
@@ -1,3 +1,6 @@
+from qarnot import connection, bucket
+import os
+
 from app.services.bucket.baseBucketService import BaseBucketService
 from app import config as cfg
 
@@ -5,10 +8,19 @@ from app import config as cfg
 class QarnotBucketService(BaseBucketService):
 
     async def upload_file(self, bucket_name: str, bucket_key: str, file_binary: bin):
-        return True
+        conn = connection.Connection(client_token=cfg.QARNOT_TOKEN)
+        media_bucket = bucket.Bucket(conn, bucket_name)
+        return await media_bucket.add_file(bucket_name, file_binary, bucket_key)
 
     async def get_uploaded_file(self, bucket_name: str, bucket_key: str):
-        return cfg.DUMMY_BUCKET_FILE
+        conn = connection.Connection(client_token=cfg.QARNOT_TOKEN)
+        media_bucket = bucket.Bucket(conn, bucket_name)
+        return await media_bucket.get_file(bucket_name, bucket_key)
 
     async def fetch_bucket_files(self, bucket_name: str):
-        return []
+        conn = connection.Connection(client_token=cfg.QARNOT_TOKEN)
+        media_bucket = bucket.Bucket(conn, bucket_name)
+        bucket_local_copy_directory = 'bucket_local_copy'
+        if not os.path.exists('bucket_local_copy_directory'):
+            os.makedirs('bucket_local_copy_directory')
+        return await media_bucket.get_all_files(bucket_local_copy_directory)
