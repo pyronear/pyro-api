3bbc12acc42c0ab9dcfc950db5fde68a689878bd
martin1tab <martin.tabikh@gmail.com>
2020-12-04 18:42:34 +0100

    Fix issues

--- src/app/services/bucket/qarnotBucketService.py
+++ src/app/services/bucket/qarnotBucketService.py
@@ -7,15 +7,15 @@ from app import config as cfg
 
 class QarnotBucketService(BaseBucketService):
 
-    def connet_to_bucket(self, bucket_name: str):
+    def _connet_to_bucket(self, bucket_name: str):
         conn = connection.Connection(client_token=cfg.QARNOT_TOKEN)
         return bucket.Bucket(conn, bucket_name)
 
     async def upload_file(self, bucket_name: str, bucket_key: str, file_binary: bin):
-        return await connet_to_bucket(bucket_name).add_file(bucket_name, file_binary, bucket_key)
+        return await _connet_to_bucket(bucket_name).add_file(bucket_name, file_binary, bucket_key)
 
     async def get_uploaded_file(self, bucket_name: str, bucket_key: str):
-        return await connet_to_bucket(bucket_name).get_file(bucket_name, bucket_key)
+        return await _connet_to_bucket(bucket_name).get_file(bucket_name, bucket_key)
 
     async def fetch_bucket_filenames(self, bucket_name: str):
-        return await connet_to_bucket(bucket_name).list_files()
+        return await _connet_to_bucket(bucket_name).list_files()
