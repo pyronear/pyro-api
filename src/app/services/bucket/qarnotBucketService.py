19639c5e3ec0de9a20a7c1aaefc7434d0652133b
martin1tab <martin.tabikh@gmail.com>
2020-12-04 18:47:21 +0100

    Fix Issues

--- src/app/services/bucket/qarnotBucketService.py
+++ src/app/services/bucket/qarnotBucketService.py
@@ -1,5 +1,4 @@
 from qarnot import connection, bucket
-import os
 
 from app.services.bucket.baseBucketService import BaseBucketService
 from app import config as cfg
@@ -7,15 +6,15 @@ from app import config as cfg
 
 class QarnotBucketService(BaseBucketService):
 
-    def _connet_to_bucket(self, bucket_name: str):
+    def _connect_to_bucket(self, bucket_name: str):
         conn = connection.Connection(client_token=cfg.QARNOT_TOKEN)
         return bucket.Bucket(conn, bucket_name)
 
     async def upload_file(self, bucket_name: str, bucket_key: str, file_binary: bin):
-        return await _connet_to_bucket(bucket_name).add_file(bucket_name, file_binary, bucket_key)
+        return await self._connect_to_bucket(bucket_name).add_file(bucket_name, file_binary, bucket_key)
 
     async def get_uploaded_file(self, bucket_name: str, bucket_key: str):
-        return await _connet_to_bucket(bucket_name).get_file(bucket_name, bucket_key)
+        return await self._connect_to_bucket(bucket_name).get_file(bucket_name, bucket_key)
 
     async def fetch_bucket_filenames(self, bucket_name: str):
-        return await _connet_to_bucket(bucket_name).list_files()
+        return await self._connect_to_bucket(bucket_name).list_files()
