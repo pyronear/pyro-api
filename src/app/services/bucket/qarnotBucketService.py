feac4f42cc0e01b91e979ad1babbd4187c7a5e8f
martin1tab <martin.tabikh@gmail.com>
2020-12-04 19:00:12 +0100

    fix issue

--- src/app/services/bucket/qarnotBucketService.py
+++ src/app/services/bucket/qarnotBucketService.py
@@ -12,10 +12,10 @@ class QarnotBucketService(BaseBucketService):
         return bucket.Bucket(conn, bucket_name)
 
     async def upload_file(self, bucket_name: str, bucket_key: str, file_binary: bin):
-        return await connect_to_bucket(bucket_name).add_file(bucket_name, file_binary, bucket_key)
+        return await self.connect_to_bucket(bucket_name).add_file(bucket_name, file_binary, bucket_key)
 
     async def get_uploaded_file(self, bucket_name: str, bucket_key: str):
-        return await connect_to_bucket(bucket_name).get_file(bucket_name, bucket_key)
+        return await self.connect_to_bucket(bucket_name).get_file(bucket_name, bucket_key)
 
     async def fetch_bucket_filenames(self, bucket_name: str):
-        return await connect_to_bucket(bucket_name).list_files()
+        return await self.connect_to_bucket(bucket_name).list_files()
