ce531033c57f9122aaf64cc4fe8d3e1af4322e87
frgfm <fgfm03@hotmail.fr>
2020-12-05 11:13:57 +0100

    style: Fixed typo

--- client/test/test_client.py
+++ client/test/test_client.py
@@ -7,7 +7,7 @@ from pyroclient.exceptions import HTTPRequestException
 
 class ClientTester(unittest.TestCase):
 
-    def _test_route_retun(self, response, return_type):
+    def _test_route_return(self, response, return_type):
         self.assertEqual(response.status_code, 200)
         self.assertIsInstance(response.json(), return_type)
 
@@ -22,11 +22,11 @@ class ClientTester(unittest.TestCase):
         api_client = client.Client("http://localhost:8002", "superuser", "superuser")
 
         # Read routes
-        self._test_route_retun(api_client.get_my_devices(), list)
-        self._test_route_retun(api_client.get_sites(), list)
-        self._test_route_retun(api_client.get_all_alerts(), list)
-        self._test_route_retun(api_client.get_ongoing_alerts(), list)
-        self._test_route_retun(api_client.get_unacknowledged_alerts(), list)
+        self._test_route_return(api_client.get_my_devices(), list)
+        self._test_route_return(api_client.get_sites(), list)
+        self._test_route_return(api_client.get_all_alerts(), list)
+        self._test_route_return(api_client.get_ongoing_alerts(), list)
+        self._test_route_return(api_client.get_unacknowledged_alerts(), list)
 
 
 if __name__ == '__main__':
