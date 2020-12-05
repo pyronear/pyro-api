b4b933f443a3f6849ef7fdc1f603bb06aa1093fe
F-G Fernandez <fgfm03@hotmail.fr>
2020-12-05 12:21:33 +0100

    test: Added unittests and typing verification for API client (#74)
    * test: Added unittest for client
    
    * chore: Added CI job for client unittests
    
    * style: Reordered imports
    
    * style: Added typing
    
    * test: Added basic unittests for client
    
    * style: Added typing
    
    * style: Added extensive typing
    
    * chore: Added mypy CI job
    
    * test: Refactored unittests
    
    * chore: Updated coverage dep in CI
    
    * style: Fixed lint
    
    * test: Removing bugged route
    
    * chore: Run API server before running client tests
    
    * style: Fixed lint
    
    * fix: Fixed shallow copy route URLs
    
    After calling the constructor with a wrong URL, the user could call it successfully with the correct URL
    
    * test: Extended tests to check deepcopy of URLs
    
    * chore: Trying to debug client unittests
    
    * style: Fixed typo

--- client/pyroclient/client.py
+++ client/pyroclient/client.py
@@ -1,12 +1,36 @@
 import requests
+from requests.models import Response
 import logging
-from .exceptions import HTTPRequestException
 from urllib.parse import urljoin
 import io
+from typing import Dict, Any
+
+from .exceptions import HTTPRequestException
+
 
 __all__ = ['Client']
 
 logging.basicConfig()
+
+ROUTES: Dict[str, str] = {
+    "token": "/login/access-token",
+    "heartbeat": "/devices/heartbeat",
+    "update-my-location": "/devices/update-my-location",
+    "create-event": "/events",
+    "send-alert": "/alerts",
+    "send-alert-from-device": "/alerts/from-device",
+    "create-media": "/media",
+    "create-media-from-device": "/media/from-device",
+    "upload-media": "/media/{media_id}/upload",
+    "get-my-devices": "/devices/my-devices",
+    "get-sites": "/sites",
+    "get-alerts": "/alerts",
+    "get-ongoing-alerts": "/alerts/ongoing",
+    "get-unacknowledged-alerts": "/alerts/unacknowledged",
+    "get-site-devices": "/installations/site-devices/{site_id}",
+    "get-media-url": "/media/{media_id}/url",
+    "get-media-image": "/media/{media_id}/image"
+}
 
 
 class Client:
@@ -18,36 +42,14 @@ class Client:
         credentials_password (str): Password (e.g: 123456 (don't do this))
     """
 
-    routes = {"token": "/login/access-token",
-              "heartbeat": "/devices/heartbeat",
-              "update-my-location": "/devices/update-my-location",
-              "create-event": "/events",
-              "send-alert": "/alerts",
-              "send-alert-from-device": "/alerts/from-device",
-              "create-media": "/media",
-              "create-media-from-device": "/media/from-device",
-              "upload-media": "/media/{media_id}/upload",
-              "get-my-devices": "/devices/my-devices",
-              "get-sites": "/sites",
-              "get-alerts": "/alerts",
-              "get-ongoing-alerts": "/alerts/ongoing",
-              "get-unacknowledged-alerts": "/alerts/unacknowledged",
-              "get-site-devices": "/installations/site-devices/{site_id}",
-              "get-media-url": "/media/{media_id}/url",
-              "get-media-image": "/media/{media_id}/image"
-              }
-
-    def __init__(self, api_url, credentials_login, credentials_password):
+    def __init__(self, api_url: str, credentials_login: str, credentials_password: str) -> None:
         self.api = api_url
-        self._add_api_url_to_routes()
+        # Prepend API url to each route
+        self.routes = {k: urljoin(self.api, v) for k, v in ROUTES.items()}
         self.token = self._retrieve_token(credentials_login, credentials_password)
         self.headers = {"Authorization": f"Bearer {self.token}"}
 
-    def _add_api_url_to_routes(self):
-        for k, v in self.routes.items():
-            self.routes[k] = urljoin(self.api, v)
-
-    def _retrieve_token(self, login, password):
+    def _retrieve_token(self, login: str, password: str) -> str:
         response = requests.post(self.routes["token"],
                                  data=f"username={login}&password={password}",
                                  headers={"Content-Type": "application/x-www-form-urlencoded",
@@ -60,12 +62,12 @@ class Client:
             raise HTTPRequestException(response.status_code, response.text)
 
     # Device functions
-    def heartbeat(self):
+    def heartbeat(self) -> Response:
         """Updates the last ping of the device"""
         return requests.put(self.routes["heartbeat"], headers=self.headers)
 
     def update_my_location(self, lat: float = None, lon: float = None,
-                           elevation: float = None, yaw: float = None, pitch: float = None):
+                           elevation: float = None, yaw: float = None, pitch: float = None) -> Response:
         """Updates the location of the device"""
         payload = {}
 
@@ -86,13 +88,13 @@ class Client:
 
         return requests.put(self.routes["update-my-location"], headers=self.headers, json=payload)
 
-    def create_event(self, lat: float, lon: float):
+    def create_event(self, lat: float, lon: float) -> Response:
         """Notify an event (e.g wildfire)."""
         payload = {"lat": lat,
                    "lon": lon}
         return requests.post(self.routes["create-event"], headers=self.headers, json=payload)
 
-    def send_alert(self, lat: float, lon: float, event_id: int, device_id: int, media_id: int = None):
+    def send_alert(self, lat: float, lon: float, event_id: int, device_id: int, media_id: int = None) -> Response:
         """Raise an alert to the API"""
         payload = {"lat": lat,
                    "lon": lon,
@@ -104,7 +106,7 @@ class Client:
 
         return requests.post(self.routes["send-alert"], headers=self.headers, json=payload)
 
-    def send_alert_from_device(self, lat: float, lon: float, event_id: int, media_id: int = None):
+    def send_alert_from_device(self, lat: float, lon: float, event_id: int, media_id: int = None) -> Response:
         """Raise an alert to the API from a device (no need to specify device ID)."""
         payload = {"lat": lat,
                    "lon": lon,
@@ -115,7 +117,7 @@ class Client:
 
         return requests.post(self.routes["send-alert-from-device"], headers=self.headers, json=payload)
 
-    def create_media(self, device_id: int):
+    def create_media(self, device_id: int) -> Response:
         """Create a media entry"""
         return requests.post(self.routes["create-media"], headers=self.headers, json={"device_id": device_id})
 
@@ -123,45 +125,45 @@ class Client:
         """Create a media entry from a device (no need to specify device ID)."""
         return requests.post(self.routes["create-media-from-device"], headers=self.headers, json={})
 
-    def upload_media(self, media_id: int, image_data: bytes):
+    def upload_media(self, media_id: int, image_data: bytes) -> Response:
         """Upload the media content"""
         return requests.post(self.routes["upload-media"].format(media_id=media_id), headers=self.headers,
                              files={'file': io.BytesIO(image_data)})
 
     # User functions
-    def get_my_devices(self):
+    def get_my_devices(self) -> Response:
         """Get the devices who are owned by the logged user"""
         return requests.get(self.routes["get-my-devices"], headers=self.headers)
 
-    def get_sites(self):
+    def get_sites(self) -> Response:
         """Get all the existing sites in the DB"""
         return requests.get(self.routes["get-sites"], headers=self.headers)
 
-    def get_all_alerts(self):
+    def get_all_alerts(self) -> Response:
         """Get all the existing alerts in the DB"""
         return requests.get(self.routes["get-alerts"], headers=self.headers)
 
-    def get_ongoing_alerts(self):
+    def get_ongoing_alerts(self) -> Response:
         """Get all the existing alerts in the DB that have the status 'start'"""
         return requests.get(self.routes["get-ongoing-alerts"], headers=self.headers)
 
-    def get_unacknowledged_alerts(self):
+    def get_unacknowledged_alerts(self) -> Response:
         """Get all the existing alerts in the DB that have the field "is_acknowledged" set to `False`"""
         return requests.get(self.routes["get-unacknowledged-alerts"], headers=self.headers)
 
-    def get_site_devices(self, site_id, payload):
+    def get_site_devices(self, site_id: int, payload: Dict[str, Any]) -> Response:
         """ Get all devices installed in a specific site"""
         return requests.get(self.routes["get-site-devices"].format(site_id=site_id), headers=self.headers, data=payload)
 
-    def get_media_url(self, media_id):
+    def get_media_url(self, media_id: int) -> Response:
         """ Get the image as a url"""
         return requests.get(self.routes["get-media-url"].format(media_id=media_id), headers=self.headers)
 
-    def get_media_url_and_read(self, media_id):
+    def get_media_url_and_read(self, media_id: int) -> Response:
         """ Get the image as a url and read it"""
         image_url = requests.get(self.routes["get-media-url"].format(media_id=media_id), headers=self.headers)
-        return requests.get(image_url)
+        return requests.get(image_url.json()['url'])
 
-    def get_media_image(self, media_id):
+    def get_media_image(self, media_id: int) -> Response:
         """ Get the image as a streaming file"""
         return requests.get(self.routes["get-media-image"].format(media_id=media_id), headers=self.headers)
