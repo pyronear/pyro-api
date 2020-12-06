563593214872fe68c4b28dd2a9a2aecb833c71e0
martin1tab <martin.tabikh@gmail.com>
2020-12-06 20:36:35 +0100

    follow up

--- src/app/services/bucket/qarnotBucketService.py
+++ src/app/services/bucket/qarnotBucketService.py
@@ -7,9 +7,10 @@ from app import config as cfg
 class QarnotBucketService(BaseBucketService):
 
     def connect_to_bucket(self, bucket_name: str):
-        if not hasattr(self, 'conn'):
-            self.conn = connection.Connection(client_token=cfg.QARNOT_TOKEN)
-        return bucket.Bucket(self.conn, bucket_name)
+        if not hasattr(self, 'bucket'):
+            conn = connection.Connection(client_token=cfg.QARNOT_TOKEN)
+            self.bucket = bucket.Bucket(conn, bucket_name)
+        return self.bucket
 
     async def upload_file(self, bucket_name: str, bucket_key: str, file_binary: bin):
         return await self.connect_to_bucket(bucket_name).add_file(bucket_name, file_binary, bucket_key)
