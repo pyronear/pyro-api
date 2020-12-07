bd26a79a42cf78ed812bf228c83b7cf0494f5cf0
F-G Fernandez <fgfm03@hotmail.fr>
2020-12-07 18:55:59 +0100

    test: Updated client unittests

--- client/test/test_client.py
+++ client/test/test_client.py
@@ -24,23 +24,20 @@ class ClientTester(unittest.TestCase):
         api_client = client.Client("http://localhost:8002", "superuser", "superuser")
 
         # Read routes
-        self._test_route_return(api_client.get_my_devices(), list)
+        all_devices = self._test_route_return(api_client.get_my_devices(), list)
         self._test_route_return(api_client.get_sites(), list)
         all_alerts = self._test_route_return(api_client.get_all_alerts(), list)
         self._test_route_return(api_client.get_ongoing_alerts(), list)
         self._test_route_return(api_client.get_unacknowledged_alerts(), list)
 
-        # Acknowledge an alert
-        alert_id = None
-        # Try to find an unacknowledged alert
-        for alert in all_alerts:
-            if not alert['is_acknowledged']:
-                alert_id = alert['id']
-        # Else it should not change the value
-        if alert_id is None and len(all_alerts) > 0:
-            alert_id = all_allerts[-1]['id']
-        if isinstance(alert_id, int):
-            updated_alert = self._test_route_return(api_client.acknowledge_alert(alert_id), dict)
+        if len(all_devices) > 0:
+            # Create event
+            event_id = self._test_route_return(api_client.create_event(0., 0.), dict)['id']
+            # Create an alert
+            device_id = all_devices[0]['id']
+            alert = self._test_route_return(api_client.send_alert(0., 0., event_id, all_devices[0]['id']), dict)
+            # Acknowledge it
+            updated_alert = self._test_route_return(api_client.acknowledge_alert(alert['id']), dict)
             self.assertTrue(updated_alert['is_acknowledged'])
 
 
