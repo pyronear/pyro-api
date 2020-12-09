3b78abb86758a5116f6d13bccf8d2bcbc318fae6
frgfm <fgfm03@hotmail.fr>
2020-12-09 10:09:06 +0100

    fix: Fixed bucket key resolution

--- src/app/services/utils.py
+++ src/app/services/utils.py
@@ -8,7 +8,7 @@ __all__ = ["prepend_bucket_folder"]
 def prepend_bucket_folder(file_name: str, bucket_folder: Optional[str] = None) -> str:
     """Prepend file name with bucket subfolder"""
 
-    if not isinstance(bucket_folder, str) and "/" in cfg.BUCKET_NAME:
+    if not isinstance(bucket_folder, str) and isinstance(cfg.BUCKET_NAME, str) and "/" in cfg.BUCKET_NAME:
         bucket_folder = cfg.BUCKET_NAME[cfg.BUCKET_NAME.find('/') + 1:]
 
     return f"{bucket_folder}/{file_name}" if isinstance(bucket_folder, str) else file_name
