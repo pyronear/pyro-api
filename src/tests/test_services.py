4eb5d565b0ea6bbd4e7c925df7bd2ae9c9d677d9
F-G Fernandez <fgfm03@hotmail.fr>
2020-12-09 21:33:41 +0100

    test: Extended unittest of bucket resolution

--- src/tests/test_services.py
+++ src/tests/test_services.py
@@ -5,18 +5,20 @@ from app.services.bucket.qarnot import cfg
 
 def test_resolve_bucket_key(monkeypatch):
     file_name = "myfile.jpg"
-    # Check that it returns the same thing when bucket folder is not set
-    assert resolve_bucket_key(file_name) == file_name
+    bucket_subfolder = "my/bucket/subfolder"
+
+    # Same if the bucket folder is specified
+    assert resolve_bucket_key(file_name, bucket_subfolder) == f"{bucket_subfolder}/{file_name}"
 
     # Check that bucket folder is prepended in bucket key if set
-    bucket_subfolder = "my/bucket/subfolder"
     origin_value = cfg.BUCKET_NAME
     monkeypatch.setattr(cfg, "BUCKET_NAME", f"my_bucket_name/{bucket_subfolder}")
     assert resolve_bucket_key(file_name) == f"{bucket_subfolder}/{file_name}"
 
-    # Same if the bucket folder is specified
+    # Check that it returns the same thing when bucket folder is not set
+    monkeypatch.setattr(cfg, "BUCKET_NAME", None)
+    assert resolve_bucket_key(file_name) == file_name
     monkeypatch.setattr(cfg, "BUCKET_NAME", origin_value)
-    assert resolve_bucket_key(file_name, bucket_subfolder) == f"{bucket_subfolder}/{file_name}"
 
 
 def test_bucket_service():
