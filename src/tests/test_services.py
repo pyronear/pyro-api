39409c8403fbf50c6c4e08136f18a379bb19bdee
F-G Fernandez <fgfm03@hotmail.fr>
2020-12-10 16:09:40 +0100

    test: Reflected changes of env vars

--- src/tests/test_services.py
+++ src/tests/test_services.py
@@ -11,14 +11,14 @@ def test_resolve_bucket_key(monkeypatch):
     assert resolve_bucket_key(file_name, bucket_subfolder) == f"{bucket_subfolder}/{file_name}"
 
     # Check that bucket folder is prepended in bucket key if set
-    origin_value = cfg.BUCKET_NAME
-    monkeypatch.setattr(cfg, "BUCKET_NAME", f"my_bucket_name/{bucket_subfolder}")
+    origin_value = cfg.BUCKET_MEDIA_FOLDER
+    monkeypatch.setattr(cfg, "BUCKET_MEDIA_FOLDER", bucket_subfolder)
     assert resolve_bucket_key(file_name) == f"{bucket_subfolder}/{file_name}"
 
     # Check that it returns the same thing when bucket folder is not set
-    monkeypatch.setattr(cfg, "BUCKET_NAME", None)
+    monkeypatch.setattr(cfg, "BUCKET_MEDIA_FOLDER", None)
     assert resolve_bucket_key(file_name) == file_name
-    monkeypatch.setattr(cfg, "BUCKET_NAME", origin_value)
+    monkeypatch.setattr(cfg, "BUCKET_MEDIA_FOLDER", origin_value)
 
 
 def test_bucket_service():
