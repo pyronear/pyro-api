bcbefc4f692181a8f184663a7d49d35492c214f1
frgfm <fgfm03@hotmail.fr>
2021-02-18 19:30:37 +0100

    chore: Added copyright & license notice

--- src/tests/utils.py
+++ src/tests/utils.py
@@ -1,3 +1,8 @@
+# Copyright (C) 2021, Pyronear contributors.
+
+# This program is licensed under the Apache License version 2.
+# See LICENSE or go to <https://www.apache.org/licenses/LICENSE-2.0.txt> for full license details.
+
 from datetime import datetime
 from copy import deepcopy
 DATETIME_FORMAT = "%Y-%m-%dT%H:%M:%S.%f"
