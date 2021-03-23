ad5dfc35a131914933a6b30af7b04fec73b0f7c0
frgfm <fgfm03@hotmail.fr>
2021-03-23 17:50:56 +0100

    test: Refactored sites route unittests

--- src/tests/routes/test_sites.py
+++ src/tests/routes/test_sites.py
@@ -9,7 +9,7 @@ from datetime import datetime
 
 from app import db
 from app.api import crud
-from tests.conf_test_db import get_entry_in_db, populate_db
+from tests.db_utils import get_entry_in_db, populate_db
 from tests.utils import update_only_datetime
 
 SITE_TABLE = [
@@ -17,6 +17,12 @@ SITE_TABLE = [
      "created_at": "2020-10-13T08:18:45.447773"},
     {"id": 2, "name": "my_first_station", "lat": 44.1, "lon": 3.9, "type": "station", "country": "FR", "geocode": "30",
      "created_at": "2020-09-13T08:18:45.447773"},
+]
+
+ACCESS_TABLE = [
+    {"id": 1, "login": "first_login", "hashed_password": "hashed_pwd", "scopes": "user"},
+    {"id": 2, "login": "second_login", "hashed_password": "hashed_pwd", "scopes": "admin"},
+    {"id": 3, "login": "third_login", "hashed_password": "hashed_pwd", "scopes": "device"},
 ]
 
 
@@ -32,45 +38,36 @@ def compare_entries(ref, test):
 SITE_TABLE_FOR_DB = list(map(update_only_datetime, SITE_TABLE))
 
 
+@pytest.fixture(scope="function")
 async def init_test_db(monkeypatch, test_db):
     monkeypatch.setattr(crud.base, "database", test_db)
+    await populate_db(test_db, db.accesses, ACCESS_TABLE)
     await populate_db(test_db, db.sites, SITE_TABLE_FOR_DB)
-
-
-@pytest.mark.asyncio
-async def test_get_site(test_app_asyncio, test_db, monkeypatch):
-
-    # Sterilize DB interactions
-    await init_test_db(monkeypatch, test_db)
-
-    response = await test_app_asyncio.get("/sites/1")
-    response_json = response.json()
-    assert response.status_code == 200
-    compare_entries(response_json, SITE_TABLE[0])
 
 
 @pytest.mark.parametrize(
     "site_id, status_code, status_details",
     [
+        [1, 200, None],
         [999, 404, "Entry not found"],
         [0, 422, None],
     ],
 )
 @pytest.mark.asyncio
-async def test_get_site_invalid(test_app_asyncio, test_db, monkeypatch, site_id, status_code, status_details):
-    # Sterilize DB interactions
-    await init_test_db(monkeypatch, test_db)
+async def test_get_site(test_app_asyncio, init_test_db, site_id, status_code, status_details):
 
     response = await test_app_asyncio.get(f"/sites/{site_id}")
-    assert response.status_code == status_code, site_id
+    response_json = response.json()
+    assert response.status_code == status_code
+
     if isinstance(status_details, str):
-        assert response.json()["detail"] == status_details
+        assert response.json()['detail'] == status_details
+    if response.status_code == 200:
+        compare_entries(response_json, SITE_TABLE[site_id - 1])
 
 
 @pytest.mark.asyncio
-async def test_fetch_sites(test_app_asyncio, test_db, monkeypatch):
-    # Sterilize DB interactions
-    await init_test_db(monkeypatch, test_db)
+async def test_fetch_sites(test_app_asyncio, init_test_db):
 
     response = await test_app_asyncio.get("/sites/")
     assert response.status_code == 200
@@ -80,108 +77,106 @@ async def test_fetch_sites(test_app_asyncio, test_db, monkeypatch):
 
 
 @pytest.mark.parametrize(
-    "test_payload, no_alert",
+    "access_idx, payload, no_alert, status_code, status_details",
     [
-        [{"name": "my_site", "lat": 0., "lon": 0., "type": "tower", "country": "FR", "geocode": "01"}, False],
-        [{"name": "my_site", "lat": 0., "lon": 0., "country": "FR", "geocode": "01"}, True],
+        [1, {"name": "my_site", "lat": 0., "lon": 0., "type": "tower", "country": "FR", "geocode": "01"}, False,
+         201, None],
+        [1, {"name": "my_site", "lat": 0., "lon": 0., "type": "station", "country": "FR", "geocode": "01"}, False,
+         201, None],
+        [1, {"name": "my_site", "lat": 0., "lon": 0., "country": "FR", "geocode": "01"}, True, 201, None],
+        [0, {"name": "my_site", "lat": 0., "lon": 0., "country": "FR", "geocode": "01"}, False,
+         401, "Permission denied"],
+        [0, {"name": "my_site", "lat": 0., "lon": 0., "country": "FR", "geocode": "01"}, True, 201, None],
+        [2, {"name": "my_site", "lat": 0., "lon": 0., "country": "FR", "geocode": "01"}, False,
+         401, "Permission denied"],
+        [1, {"names": "my_site", "lat": 0., "lon": 0., "country": "FR", "geocode": "01"}, False, 422, None],
+        [1, {"name": "my_site", "lat": 0., "country": "FR", "geocode": "01"}, False, 422, None],
     ],
 )
 @pytest.mark.asyncio
-async def test_create_site(test_app_asyncio, test_db, monkeypatch, test_payload, no_alert):
+async def test_create_site(test_app_asyncio, init_test_db, test_db,
+                           access_idx, payload, no_alert, status_code, status_details):
 
-    # Sterilize DB interactions
-    await init_test_db(monkeypatch, test_db)
+    # Create a custom access token
+    auth = await pytest.get_token(ACCESS_TABLE[access_idx]['id'], ACCESS_TABLE[access_idx]['scopes'].split())
 
-    test_response = {"id": len(SITE_TABLE) + 1, **test_payload}
+    test_response = {"id": len(SITE_TABLE) + 1, **payload}
     subroute = ""
     if no_alert:
         test_response['type'] = 'no_alert'
         subroute = "/no-alert"
 
     utc_dt = datetime.utcnow()
-    response = await test_app_asyncio.post(f"/sites{subroute}/", data=json.dumps(test_payload))
+    response = await test_app_asyncio.post(f"/sites{subroute}/", data=json.dumps(payload), headers=auth)
 
-    assert response.status_code == 201
-    json_response = response.json()
-    assert {k: v for k, v in json_response.items() if k != 'created_at'} == test_response
-    new_site_in_db = await get_entry_in_db(test_db, db.sites, json_response["id"])
-    new_site_in_db = dict(**new_site_in_db)
-    assert new_site_in_db['created_at'] > utc_dt and new_site_in_db['created_at'] < datetime.utcnow()
-
-
-@pytest.mark.parametrize(
-    "payload, status_code",
-    [
-        [{"names": "my_site", "lat": 0., "lon": 0., "type": "tower", "country": "FR", "geocode": "01"}, 422],
-        [{"name": "my_site", "lat": 0., "country": "FR", "geocode": "01"}, 422],
-    ],
-)
-@pytest.mark.asyncio
-async def test_create_site_invalid(test_app_asyncio, test_db, payload, status_code):
-    response = await test_app_asyncio.post("/sites/", data=json.dumps(payload))
     assert response.status_code == status_code
 
-
-@pytest.mark.asyncio
-async def test_update_site(test_app_asyncio, test_db, monkeypatch):
-    # Sterilize DB interactions
-    await init_test_db(monkeypatch, test_db)
-
-    test_payload = {"name": "renamed_site", "lat": 0., "lon": 0., "type": "tower", "country": "FR", "geocode": "01"}
-    response = await test_app_asyncio.put("/sites/1/", data=json.dumps(test_payload))
-    assert response.status_code == 200
-    updated_site_in_db = await get_entry_in_db(test_db, db.sites, 1)
-    updated_site_in_db = dict(**updated_site_in_db)
-    for k, v in updated_site_in_db.items():
-        assert v == test_payload.get(k, SITE_TABLE_FOR_DB[0][k])
-
-
-@pytest.mark.parametrize(
-    "site_id, payload, status_code",
-    [
-        [1, {}, 422],
-        [1, {"site_name": "foo"}, 422],
-        [999, {"name": "foo", "lat": 0., "lon": 0., "type": "tower", "country": "FR", "geocode": "01"}, 404],
-        [1, {"name": "1", "lat": 0., "lon": 0., "type": "tower", "country": "FR", "geocode": "01"}, 422],
-        [0, {"name": "foo", "lat": 0., "lon": 0., "type": "tower", "country": "FR", "geocode": "01"}, 422],
-    ],
-)
-@pytest.mark.asyncio
-async def test_update_site_invalid(test_app_asyncio, test_db, monkeypatch, site_id, payload, status_code):
-    # Sterilize DB interactions
-    await init_test_db(monkeypatch, test_db)
-
-    response = await test_app_asyncio.put(f"/sites/{site_id}/", data=json.dumps(payload))
-    assert response.status_code == status_code, print(payload)
-
-
-@pytest.mark.asyncio
-async def test_delete_site(test_app_asyncio, test_db, monkeypatch):
-    # Sterilize DB interactions
-    await init_test_db(monkeypatch, test_db)
-
-    response = await test_app_asyncio.delete("/sites/1/")
-    assert response.status_code == 200
-    compare_entries(response.json(), SITE_TABLE[0])
-
-    remaining_sites = await test_app_asyncio.get("/sites/")
-    for entry in remaining_sites.json():
-        assert entry['id'] != 1
-
-
-@pytest.mark.parametrize(
-    "site_id, status_code, status_details",
-    [
-        [999, 404, "Entry not found"],
-        [0, 422, None],
-    ],
-)
-@pytest.mark.asyncio
-async def test_delete_site_invalid(test_app_asyncio, test_db, monkeypatch, site_id, status_code, status_details):
-    # Sterilize DB interactions
-    await init_test_db(monkeypatch, test_db)
-
-    response = await test_app_asyncio.delete(f"/sites/{site_id}/")
-    assert response.status_code == status_code, print(site_id)
     if isinstance(status_details, str):
-        assert response.json()["detail"] == status_details, print(site_id)
+        assert response.json()['detail'] == status_details
+
+    if response.status_code // 100 == 2:
+        json_response = response.json()
+        assert {k: v for k, v in json_response.items() if k != 'created_at'} == test_response
+        new_site_in_db = await get_entry_in_db(test_db, db.sites, json_response["id"])
+        new_site_in_db = dict(**new_site_in_db)
+        assert new_site_in_db['created_at'] > utc_dt and new_site_in_db['created_at'] < datetime.utcnow()
+
+
+@pytest.mark.parametrize(
+    "access_idx, payload, site_id, status_code, status_details",
+    [
+        [1, {"name": "renamed_site", "lat": 0., "lon": 0., "country": "FR", "geocode": "01"}, 1, 200, None],
+        [0, {"name": "renamed_site", "lat": 0., "lon": 0., "country": "FR", "geocode": "01"}, 1,
+         401, "Permission denied"],
+        [1, {}, 1, 422, None],
+        [1, {"site_name": "foo"}, 1, 422, None],
+        [1, {"name": "foo", "lat": 0., "lon": 0., "type": "tower", "country": "FR", "geocode": "01"}, 999, 404, None],
+        [1, {"name": "1", "lat": 0., "lon": 0., "type": "tower", "country": "FR", "geocode": "01"}, 1, 422, None],
+        [1, {"name": "foo", "lat": 0., "lon": 0., "type": "tower", "country": "FR", "geocode": "01"}, 0, 422, None],
+    ],
+)
+@pytest.mark.asyncio
+async def test_update_site(test_app_asyncio, init_test_db, test_db,
+                           access_idx, payload, site_id, status_code, status_details):
+
+    # Create a custom access token
+    auth = await pytest.get_token(ACCESS_TABLE[access_idx]['id'], ACCESS_TABLE[access_idx]['scopes'].split())
+
+    response = await test_app_asyncio.put(f"/sites/{site_id}/", data=json.dumps(payload), headers=auth)
+    assert response.status_code == status_code
+
+    if isinstance(status_details, str):
+        assert response.json()['detail'] == status_details
+
+    if response.status_code // 100 == 2:
+        updated_site_in_db = await get_entry_in_db(test_db, db.sites, site_id)
+        updated_site_in_db = dict(**updated_site_in_db)
+        for k, v in updated_site_in_db.items():
+            assert v == payload.get(k, SITE_TABLE_FOR_DB[site_id - 1][k])
+
+
+@pytest.mark.parametrize(
+    "access_idx, site_id, status_code, status_details",
+    [
+        [1, 1, 200, None],
+        [0, 1, 401, "Permission denied"],
+        [1, 999, 404, "Entry not found"],
+        [1, 0, 422, None],
+    ],
+)
+@pytest.mark.asyncio
+async def test_delete_site(test_app_asyncio, init_test_db, access_idx, site_id, status_code, status_details):
+
+    # Create a custom access token
+    auth = await pytest.get_token(ACCESS_TABLE[access_idx]['id'], ACCESS_TABLE[access_idx]['scopes'].split())
+
+    response = await test_app_asyncio.delete(f"/sites/{site_id}/", headers=auth)
+    assert response.status_code == status_code
+
+    if isinstance(status_details, str):
+        assert response.json()['detail'] == status_details
+
+    if response.status_code // 100 == 2:
+        compare_entries(response.json(), SITE_TABLE[site_id - 1])
+        remaining_sites = await test_app_asyncio.get("/sites/")
+        assert all(entry['id'] != site_id for entry in remaining_sites.json())
