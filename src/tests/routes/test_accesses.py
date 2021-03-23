6c3399eba2462d045b9fc602ecb094e5b21ef01e
frgfm <fgfm03@hotmail.fr>
2021-03-23 17:50:44 +0100

    test: Refactored access route unittests

--- src/tests/routes/test_accesses.py
+++ src/tests/routes/test_accesses.py
@@ -7,49 +7,72 @@ import pytest
 
 from app import db
 from app.api import crud
-from tests.conf_test_db import populate_db
+from app.api.security import create_unlimited_access_token
+from tests.db_utils import populate_db
+
 
 ACCESS_TABLE = [
     {"id": 1, "login": "first_login", "hashed_password": "hashed_pwd", "scopes": "me"},
-    {"id": 2, "login": "second_login", "hashed_password": "hashed_pwd", "scopes": "me"},
+    {"id": 2, "login": "second_login", "hashed_password": "hashed_pwd", "scopes": "admin"},
+    {"id": 3, "login": "third_login", "hashed_password": "hashed_pwd", "scopes": "device"},
 ]
 
-
-@pytest.mark.asyncio
-async def test_get_access(test_app_asyncio, test_db, monkeypatch):
-    # Sterilize DB interactions
+@pytest.fixture(scope="function")
+async def init_test_db(monkeypatch, test_db):
     monkeypatch.setattr(crud.base, "database", test_db)
     await populate_db(test_db, db.accesses, ACCESS_TABLE)
-
-    response = await test_app_asyncio.get("/accesses/1")
-    assert response.status_code == 200
-    assert response.json() == {k: v for k, v in ACCESS_TABLE[0].items() if k != "hashed_password"}
 
 
 @pytest.mark.parametrize(
-    "access_id, status_code, status_details",
+    "access_idx, access_id, status_code, status_details",
     [
-        [999, 404, "Entry not found"],
-        [0, 422, None],
+        [0, 1, 401, "Permission denied"],
+        [0, 2, 401, "Permission denied"],
+        [1, 1, 200, None],
+        [1, 2, 200, None],
+        [1, 999, 404, "Entry not found"],
+        [1, 0, 422, None],
     ],
 )
 @pytest.mark.asyncio
-async def test_get_access_invalid(test_app_asyncio, test_db, monkeypatch, access_id, status_code, status_details):
-    monkeypatch.setattr(crud.base, "database", test_db)
-    await populate_db(test_db, db.accesses, ACCESS_TABLE)
+async def test_get_access(init_test_db, test_app_asyncio, access_idx, access_id, status_code, status_details):
 
-    response = await test_app_asyncio.get(f"/accesses/{access_id}")
-    assert response.status_code == status_code, access_id
+    # Create a custom access token
+    auth = await pytest.get_token(ACCESS_TABLE[access_idx]['id'], ACCESS_TABLE[access_idx]['scopes'].split())
+
+    response = await test_app_asyncio.get(f"/accesses/{access_id}", headers=auth)
+    assert response.status_code == status_code
+
     if isinstance(status_details, str):
-        assert response.json()["detail"] == status_details
+        assert response.json()['detail'] == status_details
+
+    if response.status_code == 200:
+        access = None
+        for _access in ACCESS_TABLE:
+            if _access['id'] == access_id:
+                access = _access
+                break
+        assert response.json() == {k: v for k, v in access.items() if k != "hashed_password"}
 
 
+@pytest.mark.parametrize(
+    "access_idx, status_code, status_details",
+    [
+        [0, 401, "Permission denied"],
+        [1, 200, None],
+    ],
+)
 @pytest.mark.asyncio
-async def test_fetch_accesses(test_app_asyncio, test_db, monkeypatch):
-    monkeypatch.setattr(crud.base, "database", test_db)
-    await populate_db(test_db, db.accesses, ACCESS_TABLE)
+async def test_fetch_accesses(init_test_db, test_app_asyncio, access_idx, status_code, status_details):
 
-    response = await test_app_asyncio.get("/accesses/")
-    assert response.status_code == 200
-    assert response.json() == [{k: v for k, v in entry.items() if k != "hashed_password"}
-                               for entry in ACCESS_TABLE]
+    # Create a custom access token
+    auth = await pytest.get_token(ACCESS_TABLE[access_idx]['id'], ACCESS_TABLE[access_idx]['scopes'].split())
+
+    response = await test_app_asyncio.get("/accesses/", headers=auth)
+    assert response.status_code == status_code
+
+    if isinstance(status_details, str):
+        assert response.json()['detail'] == status_details
+    if response.status_code == 200:
+        assert response.json() == [{k: v for k, v in entry.items() if k != "hashed_password"}
+                                   for entry in ACCESS_TABLE]
