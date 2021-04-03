de7618b536f2f464a2073a370408348e3a01ed54
frgfm <fgfm03@hotmail.fr>
2021-04-03 19:10:12 +0200

    feat: Added external HTTP request module

---
+++ src/app/api/external.py
@@ -0,0 +1,20 @@
+# Copyright (C) 2021, Pyronear contributors.
+
+# This program is licensed under the Apache License version 2.
+# See LICENSE or go to <https://www.apache.org/licenses/LICENSE-2.0.txt> for full license details.
+
+import requests
+from pydantic import BaseModel
+from typing import Any
+
+
+def post_request(url: str, payload: BaseModel) -> requests.Response:
+    """Performs a POST request to a given URL
+
+    Args:
+        url: URL to send the POST request to
+        payload: payload to be sent
+    Returns:
+        HTTP response
+    """
+    return requests.post(url, headers={"Content-Type": "application/json"}, json=payload)
