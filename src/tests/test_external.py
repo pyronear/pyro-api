593fb4e280fc23a5605515be0c366c473689d053
frgfm <fgfm03@hotmail.fr>
2021-04-03 21:58:00 +0200

    test: Fixed import

--- src/tests/test_external.py
+++ src/tests/test_external.py
@@ -3,7 +3,7 @@
 # This program is licensed under the Apache License version 2.
 # See LICENSE or go to <https://www.apache.org/licenses/LICENSE-2.0.txt> for full license details.
 
-from app.external import post_request
+from app.api.external import post_request
 
 
 def test_post_request():
