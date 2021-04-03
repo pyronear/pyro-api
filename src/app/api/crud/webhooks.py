0ed8afb962e858751c19d76975a9cdfa31d1270f
frgfm <fgfm03@hotmail.fr>
2021-04-03 19:09:54 +0200

    feat: Added crud for webhooks

---
+++ src/app/api/crud/webhooks.py
@@ -0,0 +1,19 @@
+# Copyright (C) 2021, Pyronear contributors.
+
+# This program is licensed under the Apache License version 2.
+# See LICENSE or go to <https://www.apache.org/licenses/LICENSE-2.0.txt> for full license details.
+
+from typing import List
+
+from app.api import crud
+from app.db import webhooks
+
+
+async def fetch_webhook_urls(route: str) -> List[str]:
+
+    query = (
+        select([webhooks.c.url])
+        .where(webhooks.c.callback == route)
+    )
+
+    return await crud.base.database.fetch_all(query=query)
