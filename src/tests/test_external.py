e6d80933fb9056f43de5a4c7321133b57063e96b
frgfm <fgfm03@hotmail.fr>
2021-04-03 19:18:22 +0200

    test: Added unittest for external

---
+++ src/tests/test_external.py
@@ -0,0 +1,11 @@
+# Copyright (C) 2021, Pyronear contributors.
+
+# This program is licensed under the Apache License version 2.
+# See LICENSE or go to <https://www.apache.org/licenses/LICENSE-2.0.txt> for full license details.
+
+from app.external import post_request
+
+
+def test_post_request():
+    response = post_request("https://httpbin.org/post")
+    assert response.status_code == 200
