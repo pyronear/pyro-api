e2896f214d93122558589b7a1b380dc4d91c2d30
Florian Riche <friche10@gmail.com>
2021-04-19 20:29:03 +0200

    Refactor: Use SQLAlchemy models instead of Table Declaration (#156)
    * feat: init models
    
    * feat: add get_session for SQLAlchemy
    
    * feat: migrate table meta data from tables.py  to models.py
    
    * fix: Unit Tests
    
    * feat: simplify Base.metadata and metadata
    
    * chore: flake and comments
    
    * chore: remove unusued import
    
    * chore: unusued import
    
    * chore: remove imports

--- src/app/db/tables.py
+++ src/app/db/tables.py
@@ -3,162 +3,23 @@
 # This program is licensed under the Apache License version 2.
 # See LICENSE or go to <https://www.apache.org/licenses/LICENSE-2.0.txt> for full license details.
 
-import enum
-from sqlalchemy import Column, DateTime, Integer, Float, String, Table, Enum, Boolean, ForeignKey, MetaData
-from sqlalchemy.sql import func
 
+from .session import Base
+from .models import (Users, Accesses, Alerts, Devices, Events,
+                     Groups, Installations, Media, Sites, Webhooks, EventType, AccessType, SiteType, MediaType)
 
-__all__ = ['metadata', 'SiteType', 'EventType', 'MediaType',
-           'users', 'accesses', 'groups', 'sites', 'events', 'devices', 'media', 'installations', 'alerts', 'webhooks']
+__all__ = ['metadata', 'users', 'accesses', 'groups', 'sites', 'events',
+           'devices', 'media', 'installations', 'alerts', 'webhooks']
 
+users = Users.__table__
+accesses = Accesses.__table__
+groups = Groups.__table__
+sites = Sites.__table__
+events = Events.__table__
+devices = Devices.__table__
+media = Media.__table__
+installations = Installations.__table__
+alerts = Alerts.__table__
+webhooks = Webhooks.__table__
 
-# SQLAlchemy
-# databases query builder
-metadata = MetaData()
-
-# Cores tables
-
-users = Table(
-    "users",
-    metadata,
-    Column("id", Integer, primary_key=True),
-    Column("login", String(50), unique=True),
-    Column("access_id", Integer, ForeignKey("accesses.id", ondelete="CASCADE"), unique=True),
-    Column("created_at", DateTime, default=func.now()),
-)
-
-
-class AccessType(str, enum.Enum):
-    user: str = 'user'
-    admin: str = 'admin'
-    device: str = 'device'
-
-
-accesses = Table(
-    "accesses",
-    metadata,
-    Column("id", Integer, primary_key=True),
-    Column("login", String(50), unique=True, index=True),  # index for fast lookup
-    Column("hashed_password", String(70), nullable=False),
-    Column("scope", Enum(AccessType), default=AccessType.user, nullable=False),
-    Column("group_id", Integer, ForeignKey("groups.id", ondelete="CASCADE"), nullable=False),
-)
-
-groups = Table(
-    "groups",
-    metadata,
-    Column("id", Integer, primary_key=True),
-    Column("name", String(50), unique=True),
-)
-
-
-class SiteType(str, enum.Enum):
-    tower: str = 'tower'
-    station: str = 'station'
-    no_alert: str = 'no_alert'
-
-
-sites = Table(
-    "sites",
-    metadata,
-    Column("id", Integer, primary_key=True),
-    Column("name", String(50)),
-    Column("group_id", Integer, ForeignKey("groups.id", ondelete="CASCADE"), nullable=False),
-    Column("lat", Float(4, asdecimal=True)),
-    Column("lon", Float(4, asdecimal=True)),
-    Column("country", String(5), nullable=False),
-    Column("geocode", String(10), nullable=False),
-    Column("type", Enum(SiteType), default=SiteType.tower),
-    Column("created_at", DateTime, default=func.now()),
-)
-
-
-class EventType(str, enum.Enum):
-    wildfire: str = 'wildfire'
-
-
-events = Table(
-    "events",
-    metadata,
-    Column("id", Integer, primary_key=True),
-    Column("lat", Float(4, asdecimal=True)),
-    Column("lon", Float(4, asdecimal=True)),
-    Column("type", Enum(EventType), default=EventType.wildfire),
-    Column("start_ts", DateTime, default=None, nullable=True),
-    Column("end_ts", DateTime, default=None, nullable=True),
-    Column("created_at", DateTime, default=func.now()),
-)
-
-
-# Linked tables
-devices = Table(
-    "devices",
-    metadata,
-    Column("id", Integer, primary_key=True),
-    Column("login", String(50), unique=True),
-    Column("owner_id", Integer, ForeignKey("users.id")),
-    Column("access_id", Integer, ForeignKey("accesses.id", ondelete="CASCADE"), unique=True),
-    Column("specs", String(50)),
-    Column("software_hash", String(16), default=None, nullable=True),
-    Column("angle_of_view", Float(2, asdecimal=True)),
-    Column("elevation", Float(1, asdecimal=True), default=None, nullable=True),
-    Column("lat", Float(4, asdecimal=True), default=None, nullable=True),
-    Column("lon", Float(4, asdecimal=True), default=None, nullable=True),
-    Column("yaw", Float(1, asdecimal=True), default=None, nullable=True),
-    Column("pitch", Float(1, asdecimal=True), default=None, nullable=True),
-    Column("last_ping", DateTime, default=None, nullable=True),
-    Column("created_at", DateTime, default=func.now())
-)
-
-
-class MediaType(str, enum.Enum):
-    image: str = 'image'
-    video: str = 'video'
-
-
-media = Table(
-    "media",
-    metadata,
-    Column("id", Integer, primary_key=True),
-    Column("device_id", Integer, ForeignKey("devices.id")),
-    Column("bucket_key", String(100), nullable=True),
-    Column("type", Enum(MediaType), default=MediaType.image),
-    Column("created_at", DateTime, default=func.now()),
-)
-
-
-installations = Table(
-    "installations",
-    metadata,
-    Column("id", Integer, primary_key=True),
-    Column("device_id", Integer, ForeignKey("devices.id")),
-    Column("site_id", Integer, ForeignKey("sites.id")),
-    Column("start_ts", DateTime, nullable=False),
-    Column("end_ts", DateTime, default=None, nullable=True),
-    Column("is_trustworthy", Boolean, default=True),
-    Column("created_at", DateTime, default=func.now()),
-)
-
-
-alerts = Table(
-    "alerts",
-    metadata,
-    Column("id", Integer, primary_key=True),
-    Column("device_id", Integer, ForeignKey("devices.id")),
-    Column("event_id", Integer, ForeignKey("events.id")),
-    Column("media_id", Integer, ForeignKey("media.id"), default=None),
-    Column("azimuth", Float(4, asdecimal=True), default=None),
-    Column("lat", Float(4, asdecimal=True)),
-    Column("lon", Float(4, asdecimal=True)),
-    Column("is_acknowledged", Boolean, default=False),
-    Column("created_at", DateTime, default=func.now()),
-)
-
-
-webhooks = Table(
-    "webhooks",
-    metadata,
-    Column("id", Integer, primary_key=True),
-    Column("callback", String(50), nullable=False),
-    Column("url", String(100), nullable=False),
-)
+metadata = Base.metadata
