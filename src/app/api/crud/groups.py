917ac69ae67184f9e551f4092acee26f2f564893
Unknown <friche10@gmail.com>
2021-04-30 21:22:37 +0200

    feat: handle nonexisting entries in get_entity_group_id

--- src/app/api/crud/groups.py
+++ src/app/api/crud/groups.py
@@ -40,39 +40,55 @@ async def get_entity_group_id(table: Table, entry_id: int) -> int:
 
 async def _get_access_group_id(entry_id: int) -> int:
     entry = await crud.base.get_entry(accesses, entry_id)
+    if entry is None:
+        return None
     return entry["group_id"]
 
 
 async def _get_user_group_id(entry_id: int) -> int:
     entry = await crud.base.get_entry(users, entry_id)
+    if entry is None:
+        return None
     return await _get_access_group_id(entry["access_id"])
 
 
 async def _get_site_group_id(entry_id: int) -> int:
     entry = await crud.base.get_entry(sites, entry_id)
+    if entry is None:
+        return None
     return entry["group_id"]
 
 
 async def _get_event_group_id(entry_id: int) -> int:
     entry = await crud.base.fetch_one(alerts, {"event_id": entry_id})
+    if entry is None:
+        return None
     return await _get_device_group_id(entry["device_id"])
 
 
 async def _get_device_group_id(entry_id: int) -> int:
     entry = await crud.base.get_entry(devices, entry_id)
+    if entry is None:
+        return None
     return await _get_access_group_id(entry["access_id"])
 
 
 async def _get_media_group_id(entry_id: int) -> int:
     entry = await crud.base.get_entry(media, entry_id)
+    if entry is None:
+        return None
     return await _get_device_group_id(entry["device_id"])
 
 
 async def _get_installation_group_id(entry_id: int) -> int:
     entry = await crud.base.get_entry(installations, entry_id)
+    if entry is None:
+        return None
     return await _get_site_group_id(entry["site_id"])
 
 
 async def _get_alert_group_id(entry_id: int) -> int:
     entry = await crud.base.get_entry(alerts, entry_id)
+    if entry is None:
+        return None
     return await _get_device_group_id(entry["device_id"])
