9b36ebda12f68f458c7815e549952a540d4eac12
frgfm <fgfm03@hotmail.fr>
2021-06-11 15:52:44 +0200

    test: Extended client unittests

--- client/test/test_client.py
+++ client/test/test_client.py
@@ -24,23 +24,41 @@ class ClientTester(unittest.TestCase):
         self.assertRaises(HTTPRequestException, client.Client, "http://localhost:8080", "invalid_login", "invalid_pwd")
 
         # Incorrect URL port
-        self.assertRaises(ConnectionError, client.Client, "http://localhost:8003", "superuser", "superuser")
+        self.assertRaises(ConnectionError, client.Client, "http://localhost:8003", "dummy_login", "dummy_pwd")
 
         api_client = client.Client("http://localhost:8080", "dummy_login", "dummy_pwd")
 
-        # Read routes
+        # Sites
+        site_id = self._test_route_return(
+            api_client.create_no_alert_site(
+                lat=44.870959, lon=4.395387, name="dummy_tower", country="FR", geocode="07"
+            ),
+            dict
+        )['id']
+        sites = self._test_route_return(api_client.get_sites(), list)
+        assert sites[-1]['id'] = site_id
+
+        # Devices
         all_devices = self._test_route_return(api_client.get_my_devices(), list)
-        self._test_route_return(api_client.get_sites(), list)
+        site_devices = self._test_route_return(api_client.get_site_devices(site_id), list)
+
+        # Alerts
         self._test_route_return(api_client.get_all_alerts(), list)
         self._test_route_return(api_client.get_ongoing_alerts(), list)
+        # Events
         self._test_route_return(api_client.get_unacknowledged_events(), list)
         self._test_route_return(api_client.get_past_events(), list)
 
         if len(all_devices) > 0:
+            # Media
+            media_id = self._test_route_return(api_client.create_media(all_devices[0]['id']), dict)['id']
             # Create event
             event_id = self._test_route_return(api_client.create_event(0., 0.), dict)['id']
             # Create an alert
-            _ = self._test_route_return(api_client.send_alert(0., 0., event_id, all_devices[0]['id']), dict)
+            _ = self._test_route_return(
+                api_client.send_alert(0., 0., event_id, all_devices[0]['id'], media_id=media_id),
+                dict
+            )
             # Acknowledge it
             updated_event = self._test_route_return(api_client.acknowledge_event(event_id), dict)
             self.assertTrue(updated_event['is_acknowledged'])
