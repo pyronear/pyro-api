18d352674defb87e036491b2b11f937ab0a098a4
frgfm <26927750+frgfm@users.noreply.github.com>
2022-05-18 21:09:50 +0000

    [skip ci] Documentation updates

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.2.dev0+fd1d872-git',
+    VERSION: '0.1.2.dev0+d4d7299-git',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
