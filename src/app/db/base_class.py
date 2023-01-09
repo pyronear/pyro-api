fa321f94ff18b515f771971791f8cf385924accd
F-G Fernandez <26927750+frgfm@users.noreply.github.com>
2023-01-09 23:53:59 +0100

    docs: Adds missing header

--- src/app/db/base_class.py
+++ src/app/db/base_class.py
@@ -1,3 +1,8 @@
+# Copyright (C) 2020-2023, Pyronear.
+
+# This program is licensed under the Apache License 2.0.
+# See LICENSE or go to <https://opensource.org/licenses/Apache-2.0> for full license details.
+
 from sqlalchemy.ext.declarative import declarative_base
 
 __all__ = ["Base"]
