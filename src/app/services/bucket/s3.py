9d86aef6ea9ec644547ff5e064687a0d82ec9622
F-G Fernandez <26927750+frgfm@users.noreply.github.com>
2023-01-29 22:02:04 +0100

    refactor: Removed Qarnot

--- src/app/services/bucket/s3.py
+++ src/app/services/bucket/s3.py
@@ -4,13 +4,10 @@
 # See LICENSE or go to <https://opensource.org/licenses/Apache-2.0> for full license details.
 
 import logging
-from typing import Any, Dict, Optional
+from typing import Any, Dict
 
 import boto3
-import botocore
 from fastapi import HTTPException
-
-from app import config as cfg
 
 __all__ = ["S3Bucket"]
 
@@ -19,33 +16,24 @@ logger = logging.getLogger("uvicorn.warning")
 
 
 class S3Bucket:
-    """Storage bucket manipulation object on S3 storage"""
+    """Storage bucket manipulation object on S3 storage
 
-    _s3: Optional[botocore.client.BaseClient] = None
-    _media_folder: Optional[str] = None
+    Args:
+        region: S3 region
+        endpoint_url: the S3 storage endpoint
+        access_key: the S3 access key
+        secret_key: the S3 secret key
+        bucket_name: the bucket name
+    """
 
-    def __init__(self) -> None:
-        self._connect_to_storage()
-
-    def _connect_to_storage(self) -> None:
-        """Connect to the CSP bucket"""
-        _session = boto3.Session(cfg.S3_ACCESS_KEY, cfg.S3_SECRET_KEY, region_name=cfg.S3_REGION)
-        try:
-            self._s3 = _session.client("s3", endpoint_url=cfg.S3_ENDPOINT_URL)
-        except ValueError:
-            raise ValueError(f"invalid URL: '{cfg.S3_ENDPOINT_URL}'")
-        if isinstance(cfg.BUCKET_MEDIA_FOLDER, str):
-            self._media_folder = cfg.BUCKET_MEDIA_FOLDER
-
-    @property
-    def s3(self) -> botocore.client.BaseClient:
-        if self._s3 is None:
-            self._connect_to_storage()
-        return self._s3
+    def __init__(self, region: str, endpoint_url: str, access_key: str, secret_key: str, bucket_name: str) -> None:
+        _session = boto3.Session(access_key, secret_key, region_name=region)
+        self._s3 = _session.client("s3", endpoint_url=endpoint_url)
+        self.bucket_name = bucket_name
 
     async def get_file_metadata(self, bucket_key: str) -> Dict[str, Any]:
         # https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/s3.html#S3.Client.head_object
-        return self.s3.head_object(Bucket=cfg.BUCKET_NAME, Key=bucket_key)
+        return self._s3.head_object(Bucket=self.bucket_name, Key=bucket_key)
 
     async def check_file_existence(self, bucket_key: str) -> bool:
         """Check whether a file exists on the bucket"""
@@ -63,15 +51,15 @@ class S3Bucket:
             raise HTTPException(status_code=404, detail="File cannot be found on the bucket storage")
 
         # Point to the bucket file
-        file_params = {"Bucket": cfg.BUCKET_NAME, "Key": bucket_key}
+        file_params = {"Bucket": self.bucket_name, "Key": bucket_key}
         # Generate a public URL for it using boto3 presign URL generation
-        return self.s3.generate_presigned_url("get_object", Params=file_params, ExpiresIn=url_expiration)
+        return self._s3.generate_presigned_url("get_object", Params=file_params, ExpiresIn=url_expiration)
 
     async def upload_file(self, bucket_key: str, file_binary: bytes) -> bool:
         """Upload a file to bucket and return whether the upload succeeded"""
         # https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/s3.html#S3.Bucket.upload_fileobj
         try:
-            self.s3.upload_fileobj(file_binary, cfg.BUCKET_NAME, bucket_key)
+            self._s3.upload_fileobj(file_binary, self.bucket_name, bucket_key)
         except Exception as e:
             logger.warning(e)
             return False
@@ -80,4 +68,4 @@ class S3Bucket:
     async def delete_file(self, bucket_key: str) -> None:
         """Remove bucket file and return whether the deletion succeeded"""
         # https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/s3.html#S3.Client.delete_object
-        self.s3.delete_object(Bucket=cfg.BUCKET_NAME, Key=bucket_key)
+        self._s3.delete_object(Bucket=self.bucket_name, Key=bucket_key)
--- src/app/services/services.py
+++ src/app/services/services.py
@@ -3,9 +3,10 @@
 # This program is licensed under the Apache License 2.0.
 # See LICENSE or go to <https://opensource.org/licenses/Apache-2.0> for full license details.
 
+from app import config as cfg
 from app.services.bucket import S3Bucket
 
 __all__ = ["bucket_service"]
 
 
-bucket_service = S3Bucket()
+bucket_service = S3Bucket(cfg.S3_REGION, cfg.S3_ENDPOINT_URL, cfg.S3_ACCESS_KEY, cfg.S3_SECRET_KEY, cfg.BUCKET_NAME)
