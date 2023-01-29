46eeba5cd22dc9021cbaf5749430c3030955e5b5
F-G Fernandez <26927750+frgfm@users.noreply.github.com>
2023-01-29 21:18:33 +0100

    feat: Adds safeguard errors on boot

--- src/app/config.py
+++ src/app/config.py
@@ -12,7 +12,7 @@ PROJECT_DESCRIPTION: str = "API for wildfire prevention, detection and monitorin
 API_BASE: str = "api/"
 VERSION: str = "0.2.0.dev0"
 DEBUG: bool = os.environ.get("DEBUG", "").lower() != "false"
-DATABASE_URL: str = os.getenv("DATABASE_URL", "")
+DATABASE_URL: str = os.environ["DATABASE_URL"]
 # Fix for SqlAlchemy 1.4+
 if DATABASE_URL.startswith("postgres://"):
     DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
@@ -33,21 +33,15 @@ ACCESS_TOKEN_EXPIRE_MINUTES: int = 60
 ACCESS_TOKEN_UNLIMITED_MINUTES = 60 * 24 * 365 * 10
 JWT_ENCODING_ALGORITHM = "HS256"
 
-SUPERUSER_LOGIN: str = os.getenv("SUPERUSER_LOGIN", "")
-SUPERUSER_PWD: str = os.getenv("SUPERUSER_PWD", "")
+SUPERUSER_LOGIN: str = os.environ["SUPERUSER_LOGIN"]
+SUPERUSER_PWD: str = os.environ["SUPERUSER_PWD"]
 
-if SUPERUSER_LOGIN is None or SUPERUSER_PWD is None:
-    raise ValueError(
-        "Missing Credentials. Please set 'SUPERUSER_LOGIN' and 'SUPERUSER_PWD' in your environment variables"
-    )
-
-QARNOT_TOKEN: str = os.getenv("QARNOT_TOKEN", "")
-BUCKET_NAME: str = os.getenv("BUCKET_NAME", "")
+BUCKET_NAME: str = os.environ["BUCKET_NAME"]
 BUCKET_MEDIA_FOLDER: Optional[str] = os.getenv("BUCKET_MEDIA_FOLDER")
-S3_ACCESS_KEY: str = os.getenv("S3_ACCESS_KEY", "")
-S3_SECRET_KEY: str = os.getenv("S3_SECRET_KEY", "")
-S3_REGION: str = os.getenv("S3_REGION", "")
-S3_ENDPOINT_URL: str = os.getenv("S3_ENDPOINT_URL", "")
+S3_ACCESS_KEY: str = os.environ["S3_ACCESS_KEY"]
+S3_SECRET_KEY: str = os.environ["S3_SECRET_KEY"]
+S3_REGION: str = os.environ["S3_REGION"]
+S3_ENDPOINT_URL: str = os.environ["S3_ENDPOINT_URL"]
 DUMMY_BUCKET_FILE = (
     "https://ec.europa.eu/jrc/sites/jrcsh/files/styles/normal-responsive/"
     + "public/growing-risk-future-wildfires_adobestock_199370851.jpeg"
