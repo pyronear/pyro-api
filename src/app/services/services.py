2c4e58b179406b709b76216d705a616af9c8d7ed
F-G Fernandez <26927750+frgfm@users.noreply.github.com>
2023-01-29 20:57:49 +0100

    feat: Switched to S3 storage

--- src/app/services/services.py
+++ src/app/services/services.py
@@ -3,9 +3,9 @@
 # This program is licensed under the Apache License 2.0.
 # See LICENSE or go to <https://opensource.org/licenses/Apache-2.0> for full license details.
 
-from app.services.bucket import QarnotBucket
+from app.services.bucket import S3Bucket
 
 __all__ = ["bucket_service"]
 
 
-bucket_service = QarnotBucket()
+bucket_service = S3Bucket()
