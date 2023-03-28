896f1f1c4f3fd506bc855834b28928cb95b58154
Bruno Lenzi <bruno.lenzi@developpement-durable.gouv.fr>
2023-03-28 23:13:33 +0200

    fix: header

--- src/app/api/connection_manager.py
+++ src/app/api/connection_manager.py
@@ -1,3 +1,8 @@
+# Copyright (C) 2022-2023, Pyronear.
+
+# This program is licensed under the Apache License 2.0.
+# See LICENSE or go to <https://opensource.org/licenses/Apache-2.0> for full license details.
+
 from typing import List
 
 from fastapi import WebSocket
@@ -6,7 +11,9 @@ from app.schemas import AlertOut
 
 
 class ConnectionManager:
-    """To handle connections / disconnections via websocket for multiple clients"""
+    """To handle connections / disconnections via websocket for multiple clients
+    (from https://fastapi.tiangolo.com/advanced/websockets/#handling-disconnections-and-multiple-clients)"""
+
     def __init__(self) -> None:
         self.active_connections: List[WebSocket] = []
 
