35ffe7f3cc491e3c824a252e426b5239fa692825
Bruno Lenzi <bruno.lenzi@developpement-durable.gouv.fr>
2023-07-28 12:14:05 +0200

    refactor: change alert relaxation from 5 to 30 minutes

--- src/app/api/crud/alerts.py
+++ src/app/api/crud/alerts.py
@@ -16,7 +16,7 @@ from app.schemas import AlertIn, AlertOut, EventIn
 
 
 async def resolve_previous_alert(device_id: int) -> Optional[AlertOut]:
-    # check whether there is an alert in the last 5 min by the same device
+    # check whether there is an alert in the last 30 min by the same device
     max_ts = datetime.utcnow() - timedelta(seconds=cfg.ALERT_RELAXATION_SECONDS)
     query = (
         alerts.select()
@@ -31,7 +31,7 @@ async def resolve_previous_alert(device_id: int) -> Optional[AlertOut]:
 
 
 async def create_event_if_inexistant(payload: AlertIn) -> int:
-    # check whether there is an alert in the last 5 min by the same device
+    # check whether there is an alert in the last 30 min by the same device
     previous_alert = await resolve_previous_alert(payload.device_id)
     if previous_alert is None:
         # Create an event & get the ID
