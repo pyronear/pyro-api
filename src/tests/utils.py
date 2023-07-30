95fb95872248aa0bfeb95508ea2c0aecfe83cdd3
Bruno Lenzi <bruno.lenzi@developpement-durable.gouv.fr>
2023-07-30 10:15:11 +0200

    fix: quack suggestions

--- src/tests/utils.py
+++ src/tests/utils.py
@@ -1,15 +1,14 @@
-from copy import deepcopy
 from datetime import datetime
+from typing import Any, Dict
 
 DATETIME_FORMAT = "%Y-%m-%dT%H:%M:%S.%f"
 
 
-def update_only_datetime(entity_as_dict):
-    to_return = deepcopy(entity_as_dict)
-    for key in ("created_at", "start_ts", "end_ts", "last_ping"):
-        if isinstance(to_return.get(key), str):
-            to_return[key] = parse_time(to_return[key])
-    return to_return
+def update_only_datetime(entity_as_dict: Dict[str, Any]):
+    return {
+        k: parse_time(v) if isinstance(v, str) and k in ("created_at", "start_ts", "end_ts", "last_ping") else v
+        for k, v in entity_as_dict.items()
+    }
 
 
 def parse_time(d):
