eb483f1f6bf048680df2f6cd8b6745fcf096e9bd
Bruno Lenzi <bruno.lenzi@developpement-durable.gouv.fr>
2023-10-03 16:34:43 +0200

    fix: header

--- src/app/schemas/alerts.py
+++ src/app/schemas/alerts.py
@@ -1,6 +1,7 @@
 # Copyright (C) 2020-2023, Pyronear.
 # This program is licensed under the Apache License 2.0.
 # See LICENSE or go to <https://opensource.org/licenses/Apache-2.0> for full license details.
+
 import ast
 from typing import Optional
 
