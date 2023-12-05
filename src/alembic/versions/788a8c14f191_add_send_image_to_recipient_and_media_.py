3931d590ab01ab30c4fd7c39b6ea90f5f18b1b6c
Bruno Lenzi <bruno.lenzi@developpement-durable.gouv.fr>
2023-12-05 22:49:51 +0100

    style: fix

--- src/alembic/versions/788a8c14f191_add_send_image_to_recipient_and_media_.py
+++ src/alembic/versions/788a8c14f191_add_send_image_to_recipient_and_media_.py
@@ -5,21 +5,20 @@ Revises: 92e49f7d80ae
 Create Date: 2023-12-05 16:50:59.178742
 
 """
-from alembic import op
 import sqlalchemy as sa
-
+from alembic import op
 
 # revision identifiers, used by Alembic.
-revision = '788a8c14f191'
-down_revision = '92e49f7d80ae'
+revision = "788a8c14f191"
+down_revision = "92e49f7d80ae"
 branch_labels = None
 depends_on = None
 
 
 def upgrade():
-    op.add_column('notifications', sa.Column('media_id', sa.Integer(), nullable=True))
-    op.create_foreign_key(None, 'notifications', 'media', ['media_id'], ['id'])
-    op.add_column('recipients', sa.Column('send_image', sa.Boolean(), nullable=True))
+    op.add_column("notifications", sa.Column("media_id", sa.Integer(), nullable=True))
+    op.create_foreign_key(None, "notifications", "media", ["media_id"], ["id"])
+    op.add_column("recipients", sa.Column("send_image", sa.Boolean(), nullable=True))
 
 
 def downgrade():
