bd8a5811058938ed48059806c697442bd3f20d3e
F-G Fernandez <26927750+frgfm@users.noreply.github.com>
2024-05-31 16:17:42 +0200

    refactor(client): update client codebase

--- client/tests/test_client.py
+++ client/tests/test_client.py
@@ -1,92 +1,47 @@
-import time
-from copy import deepcopy
-from urllib.parse import urljoin
-
 import pytest
-import requests
 from requests.exceptions import ConnectionError, ReadTimeout
 
 from pyroclient.client import Client
 from pyroclient.exceptions import HTTPRequestError
 
 
-def _test_route_return(response, return_type, status_code=200):
-    assert response.status_code == status_code, print(response.text)
-    assert isinstance(response.json(), return_type)
-
-    return response.json()
-
-
 @pytest.mark.parametrize(
-    ("url", "login", "pwd", "timeout", "expected_error"),
+    ("token", "host", "timeout", "expected_error"),
     [
-        # Wrong credentials
-        ("http://localhost:8080", "invalid_login", "invalid_pwd", 10, HTTPRequestError),
-        # Incorrect URL port
-        ("http://localhost:8003", "dummy_login", "dummy&P@ssw0rd!", 10, ConnectionError),
-        # Timeout
-        ("http://localhost:8080", "dummy_login", "dummy&P@ssw0rd!", 0.01, ReadTimeout),
-        # Correct
-        ("http://localhost:8080", "dummy_login", "dummy&P@ssw0rd!", 10, None),
+        ("invalid_token", "http://localhost:5050", 10, HTTPRequestError),
+        (pytest.admin_token, "http://localhost:8003", 10, ConnectionError),
+        (pytest.admin_token, "http://localhost:5050", 0.00001, ReadTimeout),
+        (pytest.admin_token, "http://localhost:5050", 10, None),
     ],
 )
-def test_client_constructor(url, login, pwd, timeout, expected_error):
+def test_client_constructor(token, host, timeout, expected_error):
     if expected_error is None:
-        api_client = Client(url, login, pwd, timeout=timeout)
-        assert isinstance(api_client.headers, dict)
+        Client(token, host, timeout=timeout)
     else:
         with pytest.raises(expected_error):
-            Client(url, login, pwd, timeout=timeout)
+            Client(token, host, timeout=timeout)
 
 
-def test_client_refresh_token(admin_client):
-    # Check token refresh
-    prev_headers = deepcopy(admin_client.headers)
-    # In case the 2nd token creation request is done in the same second, since the expiration is truncated to the
-    # second, it returns the same token
-    time.sleep(1)
-    admin_client.refresh_token("dummy_login", "dummy&P@ssw0rd!")
-    assert prev_headers != admin_client.headers
+@pytest.fixture(scope="session")
+def test_cam_workflow(cam_token, mock_img):
+    cam_client = Client(cam_token, "http://localhost:5050", timeout=10)
+    assert cam_client.heartbeat().status_code == 200
+    response = cam_client.create_detection(mock_img, 123.2)
+    assert response.status_code == 201, print(response.__dict__)
+    return response.json()["id"]
 
 
-def test_client_device(admin_client, device_client, mock_img):
-    # Every on-site interactions (critical priority)
-
-    # Get self
-    device = _test_route_return(device_client.get_self_device(), dict)
-    # Heartbeat
-    last_ping = device["last_ping"]
-    updated_device = _test_route_return(device_client.heartbeat(), dict)
-    assert isinstance(updated_device["last_ping"], str)
-    if isinstance(last_ping, str):
-        assert updated_device["last_ping"] > last_ping
-
-    # Alert
-    media_id = _test_route_return(device_client.create_media_from_device(), dict, 201)["id"]
-    _test_route_return(device_client.send_alert_from_device(1.0, 2.0, media_id, 0.0), dict, 201)
-    response = device_client.upload_media(media_id, mock_img)
-    if response.status_code == 200:
-        media = _test_route_return(response, dict)
-        assert isinstance(media["bucket_key"], str)
-        _test_route_return(admin_client.get_media_url(media_id), str)
-        # Delete media
-        response = requests.delete(
-            urljoin("http://localhost:8080", f"media/{media_id}"), headers=admin_client.headers, timeout=5
-        )
-        assert response.status_code == 200
+def test_agent_workflow(test_cam_workflow, agent_token):
+    # Agent workflow
+    agent_client = Client(agent_token, "http://localhost:5050", timeout=10)
+    response = agent_client.label_detection(test_cam_workflow, True)
+    assert response.status_code == 200, print(response.__dict__)
 
 
-def test_client_user(setup, user_client, mock_img):
-    # Every platform interaction (medium priority)
-
-    _test_route_return(user_client.get_user_devices(), list)
-    sites = _test_route_return(user_client.get_sites(), list)
-    _test_route_return(user_client.get_site_devices(sites[0]["id"]), list)
-    events = _test_route_return(user_client.get_past_events(), list)
-    events = _test_route_return(user_client.get_unacknowledged_events(), list)
-    event = _test_route_return(user_client.acknowledge_event(events[0]["id"]), dict)
-    assert event["is_acknowledged"]
-    _test_route_return(user_client.get_all_alerts(), list)
-    _test_route_return(user_client.get_ongoing_alerts(), list)
-    _test_route_return(user_client.get_alerts_for_event(events[0]["id"]), list)
-    assert user_client.get_media_url(1).status_code == 404
+def test_user_workflow(test_cam_workflow, user_token):
+    # User workflow
+    user_client = Client(user_token, "http://localhost:5050", timeout=10)
+    response = user_client.get_detection_url(test_cam_workflow)
+    assert response.status_code == 200, print(response.__dict__)
+    response = user_client.fetch_detections()
+    assert response.status_code == 200, print(response.__dict__)
