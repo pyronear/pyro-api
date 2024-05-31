5cf0ba79522121538427556af5a45d912f4d18ab
F-G Fernandez <26927750+frgfm@users.noreply.github.com>
2024-05-31 20:08:04 +0200

    test(src): revamp all tests

---
+++ src/tests/endpoints/test_login.py
@@ -0,0 +1,33 @@
+from typing import Any, Dict, Union
+
+import pytest
+from httpx import AsyncClient
+from sqlmodel.ext.asyncio.session import AsyncSession
+
+
+@pytest.mark.parametrize(
+    ("payload", "status_code", "status_detail"),
+    [
+        ({"username": "foo"}, 422, None),
+        ({"username": "foo", "password": "bar"}, 401, None),
+        ({"username": "first_login", "password": "pwd"}, 401, None),
+        ({"username": "first_login", "password": "first_pwd"}, 200, None),
+    ],
+)
+@pytest.mark.asyncio()
+async def test_login_with_creds(
+    async_client: AsyncClient,
+    user_session: AsyncSession,
+    payload: Dict[str, Any],
+    status_code: int,
+    status_detail: Union[str, None],
+):
+    response = await async_client.post("/login/creds", data=payload)
+    assert response.status_code == status_code
+    if isinstance(status_detail, str):
+        assert response.json()["detail"] == status_detail
+    if response.status_code // 100 == 2:
+        response_json = response.json()
+        assert response_json["token_type"] == "bearer"  # noqa: S105
+        assert isinstance(response_json["access_token"], str)
+        assert len(response_json["access_token"]) == 144
---
+++ src/tests/test_dependencies.py
@@ -0,0 +1,32 @@
+import pytest
+from fastapi import HTTPException
+from fastapi.security import SecurityScopes
+
+from app.api.dependencies import get_jwt
+from app.core.security import create_access_token
+
+
+@pytest.mark.parametrize(
+    ("scopes", "token", "expires_minutes", "error_code", "expected_payload"),
+    [
+        (["admin"], "", None, 406, None),
+        (["admin"], {"user_id": "123", "scopes": ["admin"]}, None, 422, None),
+        (["admin"], {"sub": "123", "scopes": ["admin"]}, -1, 401, None),
+        (["admin"], {"sub": "123", "scopes": ["admin"]}, None, None, {"sub": 123, "scopes": ["admin"]}),
+        (["agent"], {"sub": "123", "scopes": ["agent"]}, None, None, {"sub": 123, "scopes": ["agent"]}),
+        (["camera"], {"sub": "123", "scopes": ["camera"]}, None, None, {"sub": 123, "scopes": ["camera"]}),
+        (["user"], {"sub": "123", "scopes": ["user"]}, None, None, {"sub": 123, "scopes": ["user"]}),
+        (["admin"], {"sub": "123", "scopes": ["user"]}, None, 403, None),
+        (["admin"], {"sub": "123", "scopes": ["agent"]}, None, 403, None),
+        (["admin"], {"sub": "123", "scopes": ["camera"]}, None, 403, None),
+    ],
+)
+def test_get_jwt(scopes, token, expires_minutes, error_code, expected_payload):
+    _token = create_access_token(token, expires_minutes) if isinstance(token, dict) else token
+    if isinstance(error_code, int):
+        with pytest.raises(HTTPException):
+            get_jwt(SecurityScopes(scopes), _token)
+    else:
+        payload = get_jwt(SecurityScopes(scopes), _token)
+        if expected_payload is not None:
+            assert payload.model_dump() == expected_payload
