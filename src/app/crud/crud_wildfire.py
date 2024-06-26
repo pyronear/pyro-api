12858ee11cbf53f47e44c0f08e7518e1172780e6
Ronan <ronan_2@hotmail.com>
2024-06-26 16:44:34 +0200

    fix headers error

--- src/app/crud/crud_wildfire.py
+++ src/app/crud/crud_wildfire.py
@@ -1,7 +1,7 @@
-# Copyright (C) 2024, Pyronear.
+# Copyright (C) 2023-2024, Pyronear.
 
 # This program is licensed under the Apache License 2.0.
-# See LICENSE or go to <https://www.apache.org/licenses/LICENSE-2.0> for full license details
+# See LICENSE or go to <https://opensource.org/licenses/Apache-2.0> for full license details.
 
 from datetime import datetime, timedelta
 from typing import List, Optional, Tuple
