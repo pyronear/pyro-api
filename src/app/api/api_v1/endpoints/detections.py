5c492a57d6454d953c81d9f75968babcaab6dfec
Ronan <ronan_2@hotmail.com>
2024-07-10 01:30:52 +0200

    fix: linting

--- src/app/api/api_v1/endpoints/detections.py
+++ src/app/api/api_v1/endpoints/detections.py
@@ -93,6 +93,7 @@ async def create_detection(
             detail=str(e),
         )
 
+
 @router.get("/{detection_id}", status_code=status.HTTP_200_OK, summary="Fetch the information of a specific detection")
 async def get_detection(
     detection_id: int = Path(..., gt=0),
