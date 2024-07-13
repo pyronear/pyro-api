2dcab5b04a3057120624af393951aa6163f54307
F-G Fernandez <26927750+frgfm@users.noreply.github.com>
2024-07-13 11:25:10 +0200

    feat(storage): make S3 URL expiration customizable (#345)
    * feat(storage): make S3 URL expiration customizable
    
    * feat(config): make URL expiration an env var
    
    * test(storage): add test cases
    
    * style(test): remove legacy import
    
    * fix(config): fix type casting

--- src/tests/services/test_storage.py
+++ src/tests/services/test_storage.py
@@ -1,5 +1,24 @@
+import io
+
+import pytest
+from httpx import AsyncClient
+
 from app.services.storage import S3Bucket, s3_bucket
 
 
-def test_s3_bucket():
+@pytest.mark.asyncio
+async def test_s3_bucket(async_client: AsyncClient, mock_img: bytes):
     assert isinstance(s3_bucket, S3Bucket)
+    bucket_key = "logo.png"
+    url_expiration = 1
+    # Check the file does not exist
+    assert not (await s3_bucket.check_file_existence(bucket_key))
+    assert await s3_bucket.upload_file(bucket_key, io.BytesIO(mock_img))
+    assert await s3_bucket.check_file_existence(bucket_key)
+    assert isinstance(await s3_bucket.get_file_metadata(bucket_key), dict)
+    # Get the public URL
+    file_url = await s3_bucket.get_public_url(bucket_key, url_expiration)
+    assert file_url.startswith("http://")
+    # Check the file is deleted
+    await s3_bucket.delete_file(bucket_key)
+    assert not (await s3_bucket.check_file_existence(bucket_key))
