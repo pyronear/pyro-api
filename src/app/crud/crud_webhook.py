fd5564145a3612d0f1852064b6e4992321e04875
F-G Fernandez <26927750+frgfm@users.noreply.github.com>
2024-09-21 02:05:54 +0200

    style(webhook): fix typing

--- src/app/crud/crud_webhook.py
+++ src/app/crud/crud_webhook.py
@@ -4,15 +4,15 @@
 # See LICENSE or go to <https://www.apache.org/licenses/LICENSE-2.0> for full license details.
 
 
-from pydantic import AnyHttpUrl
 from sqlmodel.ext.asyncio.session import AsyncSession
 
 from app.crud.base import BaseCRUD
 from app.models import Webhook
+from app.schemas.webhooks import WebhookCreation
 
 __all__ = ["WebhookCRUD"]
 
 
-class WebhookCRUD(BaseCRUD[Webhook, AnyHttpUrl, AnyHttpUrl]):
+class WebhookCRUD(BaseCRUD[Webhook, WebhookCreation, WebhookCreation]):
     def __init__(self, session: AsyncSession) -> None:
         super().__init__(session, Webhook)
