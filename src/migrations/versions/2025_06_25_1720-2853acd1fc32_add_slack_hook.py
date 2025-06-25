88892a7548231f63a41223432bf003dee695b185
ronan <syronanm@gmail.com>
2025-06-25 17:28:48 +0200

    add migration file

---
+++ src/migrations/versions/2025_06_25_1720-2853acd1fc32_add_slack_hook.py
@@ -0,0 +1,27 @@
+"""Add Slack Hook
+
+Revision ID: 2853acd1fc32
+Revises: 4265426f8438
+Create Date: 2025-06-25 17:20:14.959429
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+import sqlmodel
+
+
+# revision identifiers, used by Alembic.
+revision: str = '2853acd1fc32'
+down_revision: Union[str, None] = '4265426f8438'
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    op.add_column("organization", sa.Column("slack_hook", sqlmodel.sql.sqltypes.AutoString(), nullable=True))
+
+def downgrade() -> None:
+    op.drop_column("organization", "slack_hook")
+
