b3aba08385e93b089b49f0f402478ddb86f97f44
MateoLOSTANLEN <mateo.lostanlen@gmail.com>
2025-09-26 09:09:46 +0200

    header

--- src/app/services/overlap.py
+++ src/app/services/overlap.py
@@ -1,7 +1,8 @@
-# Copyright (C) 2024-2025, Pyronear.
-#
+# Copyright (C) 2020-2025, Pyronear.
+
 # This program is licensed under the Apache License 2.0.
 # See LICENSE or go to <https://opensource.org/licenses/Apache-2.0> for full license details.
+
 
 from __future__ import annotations
 
