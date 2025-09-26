5f4bb77259959829541efad60c245e64b0713fe3
MateoLOSTANLEN <mateo.lostanlen@gmail.com>
2025-09-26 09:20:47 +0200

    header 2

--- src/app/schemas/sequences.py
+++ src/app/schemas/sequences.py
@@ -1,3 +1,9 @@
+# Copyright (C) 2025, Pyronear.
+
+# This program is licensed under the Apache License 2.0.
+# See LICENSE or go to <https://www.apache.org/licenses/LICENSE-2.0> for full license details.
+
+
 from datetime import datetime
 from typing import List, Tuple
 
