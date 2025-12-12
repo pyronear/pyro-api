c243e2a556e6ddd7b141987c0e9553d72f96b24d
fe51 <55736935+fe51@users.noreply.github.com>
2025-12-12 15:40:59 +0100

    mypy fixes

--- src/app/api/api_v1/endpoints/poses.py
+++ src/app/api/api_v1/endpoints/poses.py
@@ -3,7 +3,7 @@
 # This program is licensed under the Apache License 2.0.
 # See LICENSE or go to <https://www.apache.org/licenses/LICENSE-2.0> for full license details.
 
-from fastapi import APIRouter, Depends, HTTPException, Path, Security, status
+from fastapi import APIRouter, Body, Depends, HTTPException, Path, Security, status
 
 from app.api.dependencies import get_camera_crud, get_jwt, get_pose_crud
 from app.crud import CameraCRUD, PoseCRUD
@@ -17,7 +17,7 @@ router = APIRouter()
 
 @router.post("/", status_code=status.HTTP_201_CREATED, summary="Create a new pose for a camera")
 async def create_pose(
-    payload: PoseCreate,
+    payload: PoseCreate = Body(...),
     poses: PoseCRUD = Depends(get_pose_crud),
     cameras: CameraCRUD = Depends(get_camera_crud),
     token_payload: TokenPayload = Security(get_jwt, scopes=[UserRole.ADMIN, UserRole.AGENT]),
@@ -33,7 +33,8 @@ async def create_pose(
     if token_payload.organization_id != camera.organization_id and UserRole.ADMIN not in token_payload.scopes:
         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Access forbidden.")
 
-    return await poses.create(payload)
+    db_pose = await poses.create(payload)
+    return PoseRead(**db_pose.model_dump())
 
 
 @router.get("/{pose_id}", status_code=status.HTTP_200_OK, summary="Fetch information of a specific pose")
@@ -57,7 +58,7 @@ async def get_pose(
 @router.patch("/{pose_id}", status_code=status.HTTP_200_OK, summary="Update a pose")
 async def update_pose(
     pose_id: int = Path(..., gt=0),
-    payload: PoseUpdate = ...,
+    payload: PoseUpdate = Body(...),
     poses: PoseCRUD = Depends(get_pose_crud),
     cameras: CameraCRUD = Depends(get_camera_crud),
     token_payload: TokenPayload = Security(get_jwt, scopes=[UserRole.AGENT, UserRole.ADMIN]),
@@ -68,7 +69,8 @@ async def update_pose(
     if token_payload.organization_id != camera.organization_id and UserRole.ADMIN not in token_payload.scopes:
         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Access forbidden.")
 
-    return await poses.update(pose_id, payload)
+    db_pose = await poses.update(pose_id, payload)
+    return PoseRead(**db_pose.model_dump())
 
 
 @router.delete("/{pose_id}", status_code=status.HTTP_200_OK, summary="Delete a pose")
