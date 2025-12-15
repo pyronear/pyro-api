5d8a2be3a0d42089aa18287927789045fd8d10de
fe51 <55736935+fe51@users.noreply.github.com>
2025-12-15 14:26:38 +0100

    Added occlusion mask crud and schema

---
+++ src/app/crud/crud_occlusion_mask.py
@@ -0,0 +1,25 @@
+# Copyright (C) 2024-2025, Pyronear.
+
+# This program is licensed under the Apache License 2.0.
+# See LICENSE or go to <https://www.apache.org/licenses/LICENSE-2.0> for full license details.
+
+from typing import List
+
+from sqlmodel import select
+from sqlmodel.ext.asyncio.session import AsyncSession
+
+from app.crud.base import BaseCRUD
+from app.models import OcclusionMask
+from app.schemas.occlusion_masks import OcclusionMaskCreate, OcclusionMaskUpdate
+
+__all__ = ["OcclusionMaskCRUD"]
+
+
+class OcclusionMaskCRUD(BaseCRUD[OcclusionMask, OcclusionMaskCreate, OcclusionMaskUpdate]):
+    def __init__(self, session: AsyncSession) -> None:
+        super().__init__(session, OcclusionMask)
+
+    async def get_by_pose(self, pose_id: int) -> List[OcclusionMask]:
+        stmt = select(OcclusionMask).where(OcclusionMask.pose_id == pose_id)
+        results = await self.session.exec(stmt)
+        return results.all()
