a6c537a594d1bac25e31a9470bf2ec65487ffe16
Mateo <mateo.lostanlen@gmail.com>
2026-01-03 11:12:24 +0100

    headers 2026 (#540)
    * headers 2026
    
    * missing headers

--- src/app/services/storage.py
+++ src/app/services/storage.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2022-2025, Pyronear.
+# Copyright (C) 2022-2026, Pyronear.
 
 # This program is licensed under the Apache License 2.0.
 # See LICENSE or go to <https://opensource.org/licenses/Apache-2.0> for full license details.
