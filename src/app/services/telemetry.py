1540c890d71a764101f2c90b6f4a974d7fef3809
F-G Fernandez <26927750+frgfm@users.noreply.github.com>
2026-07-30 18:09:16 +0200

    fix(telemetry): adapt to PostHog 7

--- src/app/services/telemetry.py
+++ src/app/services/telemetry.py
@@ -4,7 +4,7 @@
 # See LICENSE or go to <https://opensource.org/licenses/Apache-2.0> for full license details.
 
 import logging
-from typing import Union
+from typing import Any
 
 from posthog import Posthog
 
@@ -16,23 +16,23 @@ __all__ = ["telemetry_client"]
 
 
 class TelemetryClient:
-    def __init__(self, api_key: Union[str, None] = None) -> None:
+    def __init__(self, api_key: str | None = None) -> None:
         self.is_enabled = isinstance(api_key, str)
         if isinstance(api_key, str):
-            self.ph_client = Posthog(project_api_key=api_key, host="https://eu.posthog.com")
+            self.ph_client = Posthog(project_api_key=api_key, host=settings.POSTHOG_HOST)
             logger.info("PostHog enabled")
 
-    def capture(self, *args, **kwargs) -> None:
+    def capture(self, distinct_id: str | int, *, event: str, properties: dict[str, Any] | None = None) -> None:
         if self.is_enabled:
-            self.ph_client.capture(*args, **kwargs)
+            self.ph_client.capture(event, distinct_id=distinct_id, properties=properties)
 
-    def identify(self, *args, **kwargs) -> None:
+    def identify(self, distinct_id: str | int, properties: dict[str, Any] | None = None) -> None:
         if self.is_enabled:
-            self.ph_client.identify(*args, **kwargs)
+            self.ph_client.set(distinct_id=distinct_id, properties=properties)
 
-    def alias(self, *args, **kwargs) -> None:
+    def alias(self, previous_id: str | int, distinct_id: str | int) -> None:
         if self.is_enabled:
-            self.ph_client.alias(*args, **kwargs)
+            self.ph_client.alias(previous_id=str(previous_id), distinct_id=str(distinct_id))
 
 
 telemetry_client = TelemetryClient(api_key=settings.POSTHOG_KEY)
